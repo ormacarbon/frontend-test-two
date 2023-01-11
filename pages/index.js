df19d087f20e24e30b25797aa73710da583c56af
Tales Rodrigues <tales00001@gmail.com>
2023-01-11 14:20:20 -0300

    Home page

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
+import Link from 'next/link';
 import Layout from '../components/Layout';
 
 export default function Home() {
   return (
     <Layout currentPage='home'>
-      Home
+      <h2>Are you ready to explore the universe?</h2>
+      <Link href='/explore'>Let&#039;s go!</Link>
     </Layout>
   )
 }
