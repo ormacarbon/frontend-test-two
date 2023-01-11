5914444d0b4831fb65597fc559a8629469b22009
Pedro L <pedrocontact22@gmail.com>
2023-01-11 19:18:24 -0300

    Add every possible data to Pokémon page

--- src/pages/pokemon/[id].tsx
+++ src/pages/pokemon/[id].tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import axios from "axios";
+import Image from "next/image";
 
 export const getServerSideProps = async (context: any) => {
   const id = context.params.id;
@@ -14,11 +15,104 @@ export const getServerSideProps = async (context: any) => {
 };
 
 export default function PokemonDetails({ data }: any) {
-  console.log(data);
   return (
     <>
+      <Image
+        src={data.sprites.front_default}
+        alt={data.name + "icon"}
+        width={40}
+        height={40}
+      />
       <h1>{data.name}</h1>
       <p>XP - {data.base_experience}</p>
+      <h2>Abilities</h2>
+      <div>
+        {data.abilities.map((i) => {
+          return <p key={i.ability.name}>{i.ability.name}</p>;
+        })}
+      </div>
+      <h2>Forms</h2>
+      <div>
+        {data.forms.map((i) => {
+          return <p key={i.name}>{i.name}</p>;
+        })}
+      </div>
+      <h2>Moves</h2>
+      <div>
+        {data.moves.map((i) => {
+          return <p key={i.move.name}>{i.move.name}</p>;
+        })}
+      </div>
+      <h2>Weight - {data.weight}</h2>
+      <div>
+        <h2>Gallery</h2>
+        <div>
+          {data.sprites.front_default && (
+            <Image
+              src={data.sprites.front_default}
+              alt=""
+              width={40}
+              height={40}
+            />
+          )}
+          {data.sprites.front_shiny && (
+            <Image
+              src={data.sprites.front_shiny}
+              alt=""
+              width={40}
+              height={40}
+            />
+          )}
+          {data.sprites.front_female && (
+            <Image
+              src={data.sprites.front_female}
+              alt=""
+              width={40}
+              height={40}
+            />
+          )}
+          {data.sprites.back_default && (
+            <Image
+              src={data.sprites.back_default}
+              alt=""
+              width={40}
+              height={40}
+            />
+          )}
+          {data.sprites.back_shiny && (
+            <Image
+              src={data.sprites.back_shiny}
+              alt=""
+              width={40}
+              height={40}
+            />
+          )}
+          {data.sprites.back_female && (
+            <Image
+              src={data.sprites.back_female}
+              alt=""
+              width={40}
+              height={40}
+            />
+          )}
+          {data.sprites.front_shiny_female && (
+            <Image
+              src={data.sprites.front_shiny_female}
+              alt=""
+              width={40}
+              height={40}
+            />
+          )}
+          {data.sprites.back_shiny_female && (
+            <Image
+              src={data.sprites.back_shiny_female}
+              alt=""
+              width={40}
+              height={40}
+            />
+          )}
+        </div>
+      </div>
     </>
   );
 }
