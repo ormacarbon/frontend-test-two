1e54b5e7b6177a2f8f40e826d63d854002500849
thiago nunes <desenvolvedor.nunes@gmail.com>
2023-01-11 15:51:10 -0300

    style: add style of pagination component

---
+++ src/components/pagination/styled.ts
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  gap: 0.5rem;
+  max-width: max-content;
+  margin: 0 auto;
+`;
+
+export const ToFistPage = styled.section`
+  display: flex;
+  gap: 0.5rem;
+`;
+
+export const Buttons = styled.section`
+  display: flex;
+  gap: 0.5rem;
+`;
