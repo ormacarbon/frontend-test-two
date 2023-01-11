6cf144351f9a866c26bab4f5cecfa90ace33e2ed
Carolina Margiotti <55335180+CarolinaMargiotti@users.noreply.github.com>
2023-01-11 13:03:06 -0300

    implemented styled components

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,10 +1,16 @@
+import styled from "styled-components";
+
+const NavLink = styled.a`
+	margin-right: 15px;
+`;
+
 export default function navbar() {
 	return (
 		<div>
 			<nav>
-				<a href="">Link1</a>
-				<a href="">Link2</a>
-				<a href="">Link2</a>
+				<NavLink href="/link1">Link1</NavLink>
+				<NavLink href="/link2">Link2</NavLink>
+				<NavLink href="/link3">Link3</NavLink>
 			</nav>
 		</div>
 	);
