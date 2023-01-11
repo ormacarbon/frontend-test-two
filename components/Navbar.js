41fe8519cb18f3dbbf48c1ee23aff8a694791a50
Carolina Margiotti <55335180+CarolinaMargiotti@users.noreply.github.com>
2023-01-11 13:31:44 -0300

    gave appropriate url and text

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,16 +1,18 @@
 import styled from "styled-components";
 
 const NavLink = styled.a`
-	margin-right: 15px;
+	margin-right: 5px;
 `;
 
 export default function navbar() {
 	return (
 		<div>
 			<nav>
-				<NavLink href="/link1">Link1</NavLink>
-				<NavLink href="/link2">Link2</NavLink>
-				<NavLink href="/link3">Link3</NavLink>
+				<NavLink href="/country">
+					Search universities by country
+				</NavLink>
+				<NavLink href="/name">Search university by name</NavLink>
+				<NavLink href="/domain">Search university by domain</NavLink>
 			</nav>
 		</div>
 	);
