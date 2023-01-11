fad507146b5f0e6e35ca815ad6c82303eaa6b827
Carolina Margiotti <55335180+CarolinaMargiotti@users.noreply.github.com>
2023-01-11 14:57:18 -0300

    created simple form

--- pages/country.js
+++ pages/country.js
@@ -1,3 +1,27 @@
+import React, { useState, useContext, useEffect } from "react";
+
 export default function Country() {
-	return <h1>Country</h1>;
+	const [countryName, setCountryName] = useState("");
+
+	const searchUniversities = () => {
+		//...
+	};
+
+	return (
+		<div>
+			<h3>Search universities by country</h3>
+			<form>
+				<label>Country Name</label>
+				<br />
+				<input
+					value={countryName}
+					type="text"
+					onChange={(e) => setCountryName(e.target.value)}
+				></input>
+				<br />
+				<br />
+				<button onClick={() => searchUniversities()}>Search</button>
+			</form>
+		</div>
+	);
 }
