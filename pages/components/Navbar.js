a75cdffaf5abae23b26a7cc6e6dd0bec9e900715
Caroline Maia Abreu da Silva <carolmas99@gmail.com>
2023-01-11 16:27:16 -0300

    styles: navbar styles added

--- pages/components/Navbar.js
+++ pages/components/Navbar.js
@@ -1,9 +1,12 @@
 import Link from "next/link";
+import styles from "../../styles/navbar.module.scss";
 
 export default function Navbar() {
   return (
-    <header className="navbar">
-      <Link href="/">Pokemons</Link>
-    </header>
+    <div className={styles.navbar}>
+      <Link href="/" className={styles.menu}>
+        Pokemons
+      </Link>
+    </div>
   );
 }
