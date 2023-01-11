47f59f994e8270ab04e90960075437fff42a1dee
thiago nunes <desenvolvedor.nunes@gmail.com>
2023-01-11 15:21:08 -0300

    style: initiating stylization of the showing movies page

---
+++ src/styles/pages/home.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const Main = styled.main`
+  padding: 1rem 0;
+
+  max-width: 1280px;
+  margin: 0 auto;
+
+  display: flex;
+  flex-wrap: wrap;
+
+  gap: 1rem;
+`
