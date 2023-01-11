f4847239fad6ccb7c89a766c5c59d0704009a388
Caroline Maia Abreu da Silva <carolmas99@gmail.com>
2023-01-11 15:42:04 -0300

    feat: call pokemon api

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,37 @@
+import { useEffect, useState } from "react";
+import { api } from "./api/api";
 
 export default function Home() {
+  const [pokemons, setPokemons] = useState([]);
+
+  async function getAllPokemons() {
+    try {
+      const allPokemons = await api.get("/pokemon");
+
+      setPokemons(allPokemons.data.results);
+      console.log(allPokemons.data);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  useEffect(() => {
+    getAllPokemons();
+  }, []);
+
   return (
     <div>
       <h1>Hello, World!</h1>
+
+      <div className="pokemons">
+        {pokemons.map((pokemon) => {
+          return (
+            <div className="pokemon" key={pokemon.name}>
+              <h3>{pokemon.name}</h3>
+            </div>
+          );
+        })}
+      </div>
     </div>
-  )
+  );
 }
