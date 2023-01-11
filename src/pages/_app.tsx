b4673a65dbd3d69be9c26af7a280ed2bc663fd52
thiago nunes <desenvolvedor.nunes@gmail.com>
2023-01-11 13:27:50 -0300

    feat: using header on all pages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,8 @@ import { useState } from 'react';
 import { AppProps } from 'next/app';
 import { ThemeProvider } from 'styled-components';
 
+import { Header } from '@components/header';
+
 import { theme } from '@styles/theme';
 import { GlobalStyles } from '@styles/global-styles';
 
@@ -11,6 +13,10 @@ function MyApp({ Component, pageProps }: AppProps): JSX.Element {
 
   return (
     <ThemeProvider theme={theme}>
+      <Header
+        isDarkTheme={isDarkTheme}
+        setIsDarkTheme={setIsDarkTheme}
+      />
       <Component {...pageProps} />
       <GlobalStyles isDarkTheme={isDarkTheme} />
     </ThemeProvider>
