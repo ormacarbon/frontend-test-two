6c8fb97137e310a9e91f48eff42e5ddb8ea3cf44
Caroline Maia Abreu da Silva <carolmas99@gmail.com>
2023-01-11 18:04:18 -0300

    styles: pokemonCard component styles added

--- pages/components/PokemonCard.js
+++ pages/components/PokemonCard.js
@@ -1,6 +1,8 @@
+import styles from "../../styles/pokemonCard.module.scss";
+
 export default function PokemonCard({ pokemon }) {
   return (
-    <div>
+    <div className={styles.pokemon}>
       <h3>{pokemon.name}</h3>
     </div>
   );
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { api } from "./api/api";
 import PokemonCard from "./components/PokemonCard";
+import styles from "../styles/index.module.scss";
 
 export default function Home() {
   const [pokemons, setPokemons] = useState([]);
@@ -20,14 +21,10 @@ export default function Home() {
   }, []);
 
   return (
-    <div>
-      <h1>Hello, World!</h1>
-
-      <div className="pokemons">
-        {pokemons.map((pokemon) => {
-          return <PokemonCard key={pokemon.name} pokemon={pokemon} />;
-        })}
-      </div>
+    <div className={styles.pokemons}>
+      {pokemons.map((pokemon) => {
+        return <PokemonCard key={pokemon.name} pokemon={pokemon} />;
+      })}
     </div>
   );
 }
