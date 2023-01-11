ff1013b42e176572524696874248db63f869390e
Elison Michell <dev.elisonmichell@gmail.com>
2023-01-11 17:54:13 -0300

    feat(components): add layout component

---
+++ components/layout/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Container } from './styles'
+import NavBar from '../NavBar'
+
+function Layout({ children }) {
+  return(
+    <>
+        <Container>
+          <NavBar />
+          <main>{children}</main>
+        </Container>
+    </>
+  )
+}
+export default Layout;
