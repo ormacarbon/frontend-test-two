fccbfccae36363743791223ea8811a3018445a26
Elison Michell <dev.elisonmichell@gmail.com>
2023-01-12 18:22:20 -0300

    fix(box): add theme color to background

--- components/Box/styles.js
+++ components/Box/styles.js
@@ -2,13 +2,17 @@ import styled from 'styled-components'
 
 export const Container = styled.section`
   flex: 1;
-  background: #252733;
-  padding: ${props => props.theme.spacing.xl};
-  border-radius: ${props => props.theme.spacing.lg};
+  display: flex;
+  flex-direction: column;
+  background: ${props => props.theme.colors.gray['600']};;
+  padding: ${props => props.theme.spacing['2xl']};
+  border-radius: ${props => props.theme.spacing.xl};
   box-shadow: 0 0 4rem rgba(0, 0, 0, 0.3);
+  border: 1px solid ${ props => props.theme.colors.gray[400]};
 
   h3 {
     font-size: ${props => props.theme.fonts.size.lg};
     font-weight: 500;
+    margin-bottom: ${props => props.theme.spacing.lg};
   }
 `
