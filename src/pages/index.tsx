973501546b0e6a42a2c48ee187039438c9bcf9f9
Miguel S. Barbosa <miguelbarbosacs@gmail.com>
2023-01-11 23:33:06 -0300

    [feat] home page - add PostsList to the component tree

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,31 @@
+import { AppLayout, PostCard, PostsList } from "@/components/layout";
+import { Heading } from "@/components/typography";
+import Head from "next/head";
 import styled from "styled-components";
 
-export default function Home() {
+export default function Dashboard() {
   return (
     <>
-      <Container>
-        <h1>Hello, World!</h1>
-      </Container>
+      <Head>
+        <title>My Blog</title>
+      </Head>
+
+      <AppLayout sessionTitle="My blog">
+        <HomeContainer>
+          <Heading variant="xl">Recently Published</Heading>
+
+          <PostsList />
+        </HomeContainer>
+      </AppLayout>
     </>
   );
 }
 
-const Container = styled.main`
-  background-color: ${({ theme }) => theme.colors.background1};
+const HomeContainer = styled.main`
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-top: 1rem;
+  overflow: auto;
 `;
