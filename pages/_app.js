2130c3d33125f39af55a0428af0038cb0963d6d1
Lucas-E <lucasgitacc@gmail.com>
2023-01-12 03:56:02 -0300

    Menu component ready

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,17 @@
+import { Fragment } from "react";
+import { GlobalStyle } from "../styles/global";
+import { ThemeProvider } from "styled-components";
+import lightTheme from "../styles/light";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+	return (
+		<Fragment>
+			<ThemeProvider theme={lightTheme}>
+				<GlobalStyle />
+				<Component {...pageProps} />
+			</ThemeProvider>
+		</Fragment>
+	);
 }
 
-export default MyApp
+export default MyApp;
