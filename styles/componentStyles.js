b7dd2c80fb49343a2314681e7439dd62f3b84f0d
Carolina Margiotti <55335180+CarolinaMargiotti@users.noreply.github.com>
2023-01-12 20:36:30 -0300

    styled header

---
+++ styles/componentStyles.js
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+
+export const NavLink = styled.a`
+	margin: 10px;
+	font-weight: bold;
+`;
+
+export const SpaceEvenly = styled.nav`
+	display: flex;
+	justify-content: space-evenly;
+`;
+
+export const SpaceBetween = styled.div`
+	display: flex;
+	justify-content: space-between;
+`;
+
+export const Inline = styled.div`
+	display: flex;
+	justify-content: space-around;
+`;
+
+export const Header = styled.div`
+	background-color: ${(props) => props.theme.primary};
+	border: solid;
+	border-radius: 3px;
+	padding: 10px;
+	align-items: baseline;
+`;
