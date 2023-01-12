56c2d6660fc0091f1f090335d7ec3e340008dea3
Elison Michell <dev.elisonmichell@gmail.com>
2023-01-12 18:25:27 -0300

    fix(button): fix outdated spacing

--- components/Button/styles.js
+++ components/Button/styles.js
@@ -3,14 +3,14 @@ import styled from 'styled-components'
 export const Container = styled.div`
   width: max-content;
   text-align: center;
-  gap: ${props => props.theme.spacing.md};
-  padding: ${props => props.theme.spacing.md} ${props => props.theme.spacing.xl};
+  gap: ${props => props.theme.spacing.lg};
+  padding: ${props => props.theme.spacing.lg} ${props => props.theme.spacing['2xl']};
   font-size: ${props => props.theme.fonts.size.md};
 
   background: ${props => props.theme.colors.dark};
   color: ${props => props.theme.colors.light};
   
-  border-radius: ${props => props.theme.spacing.sm};
+  border-radius: ${props => props.theme.spacing.md};
   transition: background 0.3s, color 0.3s;
 
   :hover {
