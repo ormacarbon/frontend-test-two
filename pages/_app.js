d382acb89f5b0182946a06e0fdfd9a5509d9053b
Carolina Margiotti <55335180+CarolinaMargiotti@users.noreply.github.com>
2023-01-12 10:35:36 -0300

    setup for darkmode

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,25 @@
 import Layout from "../components/layout";
 import { AppContext } from "../utils/AppContext";
+import { ThemeProvider } from "styled-components";
+import { lightTheme, darkTheme, GlobalStyles } from "../styles/ThemeConfig";
+import { useState } from "react";
 
 function MyApp({ Component, pageProps }) {
+	const [theme, setTheme] = useState("light");
+
+	const toggleTheme = () => {
+		theme == "light" ? setTheme("dark") : setTheme("light");
+	};
+
 	return (
-		<Layout>
-			<AppContext>
-				<Component {...pageProps} />
-			</AppContext>
-		</Layout>
+		<ThemeProvider theme={theme == "light" ? lightTheme : darkTheme}>
+			<GlobalStyles />
+			<Layout>
+				<AppContext>
+					<Component {...pageProps} />
+				</AppContext>
+			</Layout>
+		</ThemeProvider>
 	);
 }
 
