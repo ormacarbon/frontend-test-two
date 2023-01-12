dfef31598c7ad104463afb3b7feeb95e2565f0b6
= <f7lipe@me.com>
2023-01-12 16:33:54 -0300

    misc: shared styles
    Add reusable styled components for the project

---
+++ shared/styles/sharedStyles.jsx
@@ -0,0 +1,78 @@
+import styled from "styled-components";
+
+const HStack = styled.section`
+    display: flex;
+    flex-direction: row;
+    flex-wrap: ${(props) => props.wrap || "nowrap"};
+    width: ${(props) => props.width || "100%"};
+    height: ${(props) => props.height || "100%"};
+    margin: ${(props) => props.margin || "0"};
+    padding: ${(props) => props.padding || "0"};
+    align-items: ${(props) => props.align || "center"};
+    justify-content: ${(props) => props.justify || "center"};
+    border: ${(props) => props.border || "none"};
+
+    @media (max-width: 768px) {
+        width: ${(props) => props.widthMobile};
+    }
+`
+const VStack = styled.section`
+    display: flex;
+    flex-direction: column;
+    flex-wrap: ${(props) => props.wrap || "nowrap"};
+    width: ${(props) => props.width || "100%"};
+    height: ${(props) => props.height || "100%"};
+    margin: ${(props) => props.margin || "0"};
+    padding: ${(props) => props.padding || "0"};
+    align-items: ${(props) => props.align || "center"};
+    justify-content: ${(props) => props.justify || "center"};
+    border: ${(props) => props.border || "none"};
+    border-radius: ${(props) => props.borderRadius || "0"};
+    background: ${(props) => props.background || "none"};
+
+    @media (max-width: 768px) {
+        width: ${(props) => props.widthMobile};
+        justify-content: ${(props) => props.mobileJustify};
+        align-items: ${(props) => props.mobileAlign};
+    }
+`
+const Container = styled.div`
+  padding: 0 0.5rem;
+  display: flex;
+  flex-flow: column nowrap;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  min-height: 100vh;
+`
+const Main = styled.main`
+  padding: 5rem 0;
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
+const Title = styled.h1`
+  margin: 0;
+  line-height: 1.15;
+  font-size: 4rem;
+  text-align: center;
+  text-decoration: none;
+
+  a {
+    color: ${({ theme }) => theme.colors.secondary};
+    text-decoration: none;
+    &:hover,
+    :focus,
+    :active {
+      text-decoration: underline;
+    }
+  }
+`
+const Description = styled.p`
+  text-align: center;
+  line-height: 1.5;
+  font-size: 1.5rem;
+`
+export { HStack, VStack, Container, Main, Title, Description}
