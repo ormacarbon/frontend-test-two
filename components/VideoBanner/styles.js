856616ff0d49c51110ab76b3f70c104145ffef53
Elison Michell <dev.elisonmichell@gmail.com>
2023-01-12 18:36:39 -0300

    fix(video banner): update outdated spacing

--- components/VideoBanner/styles.js
+++ components/VideoBanner/styles.js
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 export const Container = styled.div`
   position: relative;
   display: flex;
-  border-radius: ${props => props.theme.spacing.lg};
+  border-radius: ${props => props.theme.spacing.xl};
   overflow-y: hidden;
 
   video {
@@ -16,13 +16,13 @@ export const Container = styled.div`
 
 export const Content = styled.div`
   z-index: 1;
-  padding: ${props => props.theme.spacing['3xl']};
+  padding: ${props => props.theme.spacing['4xl']};
 
   h4 {
-    font-size: ${props => props.theme.fonts.size.xl};
+    font-size: ${props => props.theme.fonts.size['2xl']};
   }
 
   & :nth-child(3) {
-    margin-top: ${props => props.theme.spacing.lg};
+    margin-top: ${props => props.theme.spacing.xl};
   }
 `
