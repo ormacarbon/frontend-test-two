3c95b749d3e641732df24d1739d33f5f2c23df3d
Caroline Maia Abreu da Silva <carolmas99@gmail.com>
2023-01-12 10:49:49 -0300

    styles: pokemon Card styles added

--- components/PokemonCard.js
+++ components/PokemonCard.js
@@ -1,11 +1,13 @@
 import Link from "next/link";
-import { Pokemon } from "../styles/pokemonCard";
+import { Pokemon, DetailButton } from "../styles/pokemonCard";
 
 export default function PokemonCard({ pokemon }) {
   return (
     <Pokemon>
       <h3>{pokemon.name}</h3>
-      <Link href={`pokemon/${pokemon.name}`}>More Details</Link>
+      <DetailButton>
+        <Link href={`pokemon/${pokemon.name}`}>More Details</Link>
+      </DetailButton>
     </Pokemon>
   );
 }
--- styles/pokemonCard.js
+++ styles/pokemonCard.js
@@ -1,7 +1,22 @@
 import styled from "styled-components";
 
 export const Pokemon = styled.div`
-  padding: 3rem;
+  padding: 2rem 1rem;
   border: 2px solid #375caa;
   text-align: center;
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+`;
+
+export const DetailButton = styled.div`
+  padding: 0.3rem 0.3rem;
+  background: #375caa;
+  border-radius: 8px;
+  cursor: pointer;
+  margin: 0 auto;
+  a {
+    color: #fff;
+    text-decoration: none;
+  }
 `;
