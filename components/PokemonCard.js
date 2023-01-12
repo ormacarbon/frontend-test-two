70c5129f978bea2461b6dcf052c7d004f1cb4238
Caroline Maia Abreu da Silva <carolmas99@gmail.com>
2023-01-12 09:58:09 -0300

    feat: internal pokemon page created

--- components/PokemonCard.js
+++ components/PokemonCard.js
@@ -1,13 +1,12 @@
 import Link from "next/link";
+import { api } from "../pages/api/api";
 import styles from "../styles/pokemonCard.module.scss";
 
 export default function PokemonCard({ pokemon }) {
   return (
     <div className={styles.pokemon}>
       <h3>{pokemon.name}</h3>
-      <Link href={`pokemon/${pokemon.name}`} pokemon={pokemon}>
-        Detalhes
-      </Link>
+      <Link href={`pokemon/${pokemon.name}`}>More Details</Link>
     </div>
   );
 }
--- pages/pokemon/[pokemonName].js
+++ pages/pokemon/[pokemonName].js
@@ -1,3 +1,48 @@
+import Image from "next/image";
+import { api } from "../api/api";
+
+export const getStaticPaths = async () => {
+  const allPokemons = await api.get("/pokemon");
+
+  const paths = allPokemons.data.results.map((pokemon) => {
+    return {
+      params: {
+        pokemonName: pokemon.name,
+      },
+    };
+  });
+
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export const getStaticProps = async (context) => {
+  const name = context.params.pokemonName;
+
+  const res = await api.get(`/pokemon/${name}`);
+
+  return {
+    props: {
+      pokemon: res.data,
+    },
+  };
+};
+
 export default function Pokemon({ pokemon }) {
-  return <p>Testando</p>;
+  console.log("estamos pegando a api", pokemon);
+  console.log("habilidade", pokemon.abilities[1].ability.url);
+
+  return (
+    <div>
+      <Image
+        src={pokemon.sprites.front_default}
+        width="150"
+        height="150"
+        alt={pokemon.name}
+      />
+      <p>{pokemon.name}</p>
+    </div>
+  );
 }
