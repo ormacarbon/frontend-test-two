27fd8669fadbb94e33dda424224b510a5672dbbf
Caroline Maia Abreu da Silva <carolmas99@gmail.com>
2023-01-12 10:35:21 -0300

    styles: navbar  with styled components added

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,12 +1,12 @@
 import Link from "next/link";
-import styles from "../styles/navbar.module.scss";
+import { NavbarMenu, Menu } from "../styles/navbar";
 
 export default function Navbar() {
   return (
-    <div className={styles.navbar}>
-      <Link href="/" className={styles.menu}>
-        Pokemons
-      </Link>
-    </div>
+    <NavbarMenu>
+      <Menu>
+        <Link href="/">Pokemons</Link>
+      </Menu>
+    </NavbarMenu>
   );
 }
--- styles/navbar.js
+++ styles/navbar.js
@@ -1,15 +1,19 @@
-.navbar {
+import styled from "styled-components";
+
+export const NavbarMenu = styled.div`
   background-color: #ffcc01;
-  padding: 2rem;
   border-bottom: 0.4rem solid #375caa;
   display: flex;
   justify-content: center;
   margin-bottom: 3rem;
+`;
 
-  .menu {
+export const Menu = styled.div`
+  padding: 2rem;
+  a {
     color: #375caa;
     text-decoration: none;
     font-weight: 700;
     font-size: 1.6rem;
   }
-}
+`;
