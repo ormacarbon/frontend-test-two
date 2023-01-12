3b1d2210ceeff227440f2be8e673a2ed95ad4072
Pedro L <pedrocontact22@gmail.com>
2023-01-12 14:22:54 -0300

    Add styling for Pokémon details page

--- src/pages/pokemon/[id].tsx
+++ src/pages/pokemon/[id].tsx
@@ -16,103 +16,149 @@ export const getServerSideProps = async (context: any) => {
 
 export default function PokemonDetails({ data }: any) {
   return (
-    <>
-      <Image
-        src={data.sprites.front_default}
-        alt={data.name + "icon"}
-        width={400}
-        height={400}
-      />
-      <h1 className="text-center capitalize text-4xl font-bold">{data.name}</h1>
-      <p className="text-center text-2xl">{data.base_experience} XP</p>
-      <h2>Abilities</h2>
-      <div>
-        {data.abilities.map((i) => {
-          return <p key={i.ability.name}>{i.ability.name}</p>;
-        })}
-      </div>
-      <h2>Forms</h2>
-      <div>
-        {data.forms.map((i) => {
-          return <p key={i.name}>{i.name}</p>;
-        })}
-      </div>
-      <h2>Moves</h2>
-      <div>
-        {data.moves.map((i) => {
-          return <p key={i.move.name}>{i.move.name}</p>;
-        })}
-      </div>
-      <h2>Weight - {data.weight}</h2>
-      <div>
-        <h2>Gallery</h2>
-        <div>
-          {data.sprites.front_default && (
-            <Image
-              src={data.sprites.front_default}
-              alt=""
-              width={40}
-              height={40}
-            />
-          )}
-          {data.sprites.front_shiny && (
-            <Image
-              src={data.sprites.front_shiny}
-              alt=""
-              width={40}
-              height={40}
-            />
-          )}
-          {data.sprites.front_female && (
-            <Image
-              src={data.sprites.front_female}
-              alt=""
-              width={40}
-              height={40}
-            />
-          )}
-          {data.sprites.back_default && (
-            <Image
-              src={data.sprites.back_default}
-              alt=""
-              width={40}
-              height={40}
-            />
-          )}
-          {data.sprites.back_shiny && (
-            <Image
-              src={data.sprites.back_shiny}
-              alt=""
-              width={40}
-              height={40}
-            />
-          )}
-          {data.sprites.back_female && (
-            <Image
-              src={data.sprites.back_female}
-              alt=""
-              width={40}
-              height={40}
-            />
-          )}
-          {data.sprites.front_shiny_female && (
-            <Image
-              src={data.sprites.front_shiny_female}
-              alt=""
-              width={40}
-              height={40}
-            />
-          )}
-          {data.sprites.back_shiny_female && (
-            <Image
-              src={data.sprites.back_shiny_female}
-              alt=""
-              width={40}
-              height={40}
-            />
-          )}
-        </div>
-      </div>
-    </>
+    <div className="bg-gray-200 flex w-[100vw] -mb-6">
+      <main className="mt-5 mx-auto">
+        <section className="bg-white w-[400px] rounded-xl shadow pb-4">
+          <Image
+            src={data.sprites.front_default}
+            alt={data.name + "icon"}
+            width={400}
+            height={400}
+          />
+          <h1 className="text-center capitalize text-4xl font-bold">
+            {data.name}
+          </h1>
+          <p className="text-center text-2xl">{data.base_experience} XP</p>
+        </section>
+        <section className="bg-white mt-5 w-[400px] rounded-xl shadow py-4">
+          <h2 className="text-2xl font-bold text-center">Abilities</h2>
+          {data.abilities.map((i) => {
+            return (
+              <p
+                key={i.ability.name}
+                className="text-center text-xl capitalize"
+              >
+                -{i.ability.name.replace(/-/g, " ")}
+              </p>
+            );
+          })}
+        </section>
+        <section className="bg-white mt-5 w-[400px] rounded-xl shadow py-4">
+          <h2 className="text-center text-2xl font-bold">Moves</h2>
+          {data.moves.map((i) => {
+            return (
+              <p key={i.move.name} className="text-center text-xl capitalize">
+                -{i.move.name.replace(/-/g, " ")}
+              </p>
+            );
+          })}
+        </section>
+        <section className="bg-white mt-5 w-[400px] rounded-xl shadow py-4">
+          <h2 className="text-center text-2xl font-bold">Weight</h2>
+          <p className="text-center text-xl">{data.weight} Hg</p>
+        </section>
+        <section className="bg-white mt-5 w-[400px] rounded-xl shadow py-4 mb-5">
+          <h2 className="text-center text-2xl font-bold">Gallery</h2>
+          <div className="flex">
+            <div className="mx-auto grid grid-cols-3">
+              {data.sprites.front_default && (
+                <div>
+                  <Image
+                    src={data.sprites.front_default}
+                    alt={data.name + "front default"}
+                    width={300}
+                    height={300}
+                  />
+                  <p className="text-center">Front default</p>
+                </div>
+              )}
+              {data.sprites.front_shiny && (
+                <div>
+                  <Image
+                    src={data.sprites.front_shiny}
+                    alt={data.name + "front shiny"}
+                    width={300}
+                    height={300}
+                  />
+                  <p className="text-center">Front shiny</p>
+                </div>
+              )}
+              {data.sprites.front_female && (
+                <div>
+                  <Image
+                    src={data.sprites.front_female}
+                    alt={data.name + "front female"}
+                    width={300}
+                    height={300}
+                  />
+                  <p className="text-center">Front female</p>
+                </div>
+              )}
+              {data.sprites.back_default && (
+                <div>
+                  <Image
+                    src={data.sprites.back_default}
+                    alt={data.name + "back default"}
+                    width={300}
+                    height={300}
+                  />
+                  <p className="text-center">Back default</p>
+                </div>
+              )}
+              {data.sprites.back_shiny && (
+                <div>
+                  <Image
+                    src={data.sprites.back_shiny}
+                    alt={data.name + "back shiny"}
+                    width={300}
+                    height={300}
+                  />
+                  <p className="text-center">Back shiny</p>
+                </div>
+              )}
+              {data.sprites.back_female && (
+                <div>
+                  <Image
+                    src={data.sprites.back_female}
+                    alt={data.name + "back female"}
+                    width={300}
+                    height={300}
+                  />
+                  <p className="text-center">Back female</p>
+                </div>
+              )}
+              {data.sprites.front_shiny_female && (
+                <div>
+                  <Image
+                    src={data.sprites.front_shiny_female}
+                    alt={data.name + "front shiny female"}
+                    width={300}
+                    height={300}
+                  />
+                  <p className="text-center">
+                    Front shiny <br />
+                    female
+                  </p>
+                </div>
+              )}
+              {data.sprites.back_shiny_female && (
+                <div>
+                  <Image
+                    src={data.sprites.back_shiny_female}
+                    alt={data.name + "back shiny female"}
+                    width={300}
+                    height={300}
+                  />
+                  <p className="text-center">
+                    Back shiny <br />
+                    female
+                  </p>
+                </div>
+              )}
+            </div>
+          </div>
+        </section>
+      </main>
+    </div>
   );
 }
