2adc0efca8849f9d4c45a6f2798f573e62550fba
Elison Michell <dev.elisonmichell@gmail.com>
2023-01-12 18:41:03 -0300

    feat(small card): add small pokemon card component

---
+++ components/SmallCard/index.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Container, Ring, Content, Header, Types } from './styles'
+
+import TypeLabel from '../TypeLabel'
+import Image from 'next/image';
+
+function SmallCard(props) {
+  return(
+    <Container active={props.active} onClick={props.onClick}>
+      <Ring>
+        {
+          props.sprite && <Image src={props.sprite} alt={props.name + " Sprite"} width={360} height={360}/>
+        }
+      </Ring>
+      <Content>
+        <Header>
+          <span>{props.name}</span>
+          <div>Nº&nbsp;{props.id}</div>
+        </Header>
+        <Types>
+          {props.type && props.type.map((type, id) => <TypeLabel key={type} type={props.type[id]}/>)}
+        </Types>
+      </Content>
+    </Container>
+  )
+}
+export default SmallCard;
---
+++ components/SmallCard/styles.js
@@ -0,0 +1,62 @@
+import styled from 'styled-components'
+
+export const Container = styled.button`
+  flex: 1;
+  display: flex;
+  gap: ${props => props.theme.spacing.sm};
+  padding: ${props => props.theme.spacing.lg};
+  border-radius: ${props => props.theme.spacing.xl};
+  color: ${ props => props.theme.colors.light };
+  background: ${props => props.active && props.theme.colors.gray[400]};
+  border: 1px solid ${ props => props.active ? props.theme.colors.gray[300] : props.theme.colors.gray[400]};
+  transition: background 0.3s, border 0.3s;
+  margin-bottom: ${props => props.theme.spacing.sm};
+
+  :hover {
+    background: ${props => !props.active && props.theme.colors.gray[500]};
+  }
+`
+
+export const Ring = styled.div`
+  width: 5rem;
+  height: 5rem;
+  border-radius: 50%;
+  background: ${ props => props.theme.colors.gray[400]};
+  border: 2px solid ${ props => props.theme.colors.gray[300]};
+  overflow: hidden;
+  img {
+    width: 100%;
+    height: 100%;
+  }
+`
+
+export const Header = styled.header`
+  display: flex;
+  align-items: flex-start;
+  justify-content: space-between;
+  font-weight: 500;
+
+  span {
+    font-size: ${ props => props.theme.fonts.size.xl };
+  }
+
+  div {
+    padding: ${ props => props.theme.spacing.xs } ${ props => props.theme.spacing.sm };
+    background: ${ props => props.theme.colors.label};
+    font-size: ${ props => props.theme.fonts.size.xs };
+    color: ${ props => props.theme.colors.light};
+    border-radius: ${ props => props.theme.spacing.lg};
+  }
+`
+
+export const Content = styled.div`
+  flex: 1;
+  display: flex;
+  gap: ${ props => props.theme.spacing.sm };
+  flex-direction: column;
+`
+
+export const Types = styled.div`
+  display: flex;
+  gap: ${ props => props.theme.spacing.xs };
+`
