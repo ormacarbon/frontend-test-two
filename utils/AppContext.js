e91cc31694a0db15ae660d1b3c13122ff5065e2c
Carolina Margiotti <55335180+CarolinaMargiotti@users.noreply.github.com>
2023-01-12 11:23:13 -0300

    setup dark mode feature

--- utils/AppContext.js
+++ utils/AppContext.js
@@ -1,25 +1,5 @@
 import React, { createContext } from "react";
-import hooks from "./hooks";
 
 const Context = createContext();
 
-function AppContext({ children }) {
-	const {
-		searchUniversitiesByCountry,
-		searchUniversitiesByDomain,
-		searchUniversitiesByName,
-	} = hooks();
-	return (
-		<Context.Provider
-			value={{
-				searchUniversitiesByCountry,
-				searchUniversitiesByDomain,
-				searchUniversitiesByName,
-			}}
-		>
-			{children}
-		</Context.Provider>
-	);
-}
-
-export { Context, AppContext };
+export { Context };
