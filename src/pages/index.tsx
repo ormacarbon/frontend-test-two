4e6bb891ea58e11a9c98f02542e62557595f425a
Pedro L <pedrocontact22@gmail.com>
2023-01-12 17:04:08 -0300

    Add pagination

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,14 +12,16 @@ type PokemonListType = {
 };
 
 export default function Home() {
-  // It's better to use React Query than useEffect and useState for API calls
-  const { data, isFetching } = useQuery("pokémon list", async () => {
-    const response = await axios.get("https://pokeapi.co/api/v2/pokemon");
+  const [page, setPage] = useState(1);
+  const [grid, setGrid] = useState(true);
 
+  // It's better to use React Query than useEffect and useState for API calls
+  const { data, isFetching } = useQuery(["pokémon list", page], async () => {
+    const response = await axios.get(
+      `https://pokeapi.co/api/v2/pokemon?offset=${(page - 1) * 20}&limit=20`
+    );
     return response.data;
   });
-
-  const [grid, setGrid] = useState(true);
 
   return (
     <div className="bg-gray-200 w-screen min-h-screen -mb-6">
@@ -28,14 +30,14 @@ export default function Home() {
         {grid ? (
           <button
             onClick={() => setGrid(!grid)}
-            className="bg-gray-500 hover:bg-gray-600 active:bg-gray-700 py-3 px-5 text-white font-semibold rounded-lg transition-all duration-250 ease-in"
+            className="bg-gray-400 hover:bg-gray-500 active:bg-gray-600 py-3 px-5 text-white font-semibold rounded-lg transition-all duration-250 ease-in"
           >
             <BsListUl />
           </button>
         ) : (
           <button
             onClick={() => setGrid(!grid)}
-            className="bg-gray-500 hover:bg-gray-600 active:bg-gray-700 py-3 px-5 text-white font-semibold rounded-lg transition-all duration-250 ease-in"
+            className="bg-gray-400 hover:bg-gray-500 active:bg-gray-600 py-3 px-5 text-white font-semibold rounded-lg transition-all duration-250 ease-in"
           >
             <BsGrid3X3 />
           </button>
@@ -64,22 +66,77 @@ export default function Home() {
           )}
         </div>
       ) : (
-        <ul className="mt-1">
-          {isFetching ? (
-            <Image
-              src="https://media.tenor.com/On7kvXhzml4AAAAj/loading-gif.gif"
-              alt="loading pokémon list"
-              width={100}
-              height={100}
-              className="flex mx-auto my-[25vh]"
-            />
-          ) : (
-            data?.results?.map((pokemon: PokemonListType) => {
-              return <PokemonList key={pokemon.name} pokemonData={pokemon} />;
-            })
-          )}
-        </ul>
+        <div className="mt-1">
+          <ul>
+            {isFetching ? (
+              <Image
+                src="https://media.tenor.com/On7kvXhzml4AAAAj/loading-gif.gif"
+                alt="loading pokémon list"
+                width={100}
+                height={100}
+                className="flex mx-auto my-[25vh]"
+              />
+            ) : (
+              data?.results?.map((pokemon: PokemonListType) => {
+                return <PokemonList key={pokemon.name} pokemonData={pokemon} />;
+              })
+            )}
+          </ul>
+        </div>
       )}
+
+      <div className="flex justify-center py-5 gap-1">
+        <button
+          onClick={() => setPage(1)}
+          className={` py-3 px-5 text-white font-semibold rounded transition-all duration-250 ease-in ${
+            page === 1
+              ? "bg-gray-500 hover:bg-gray-600 active:bg-gray-700"
+              : "bg-gray-400 hover:bg-gray-500 active:bg-gray-600"
+          }`}
+        >
+          1
+        </button>
+        <button
+          onClick={() => setPage(2)}
+          className={` py-3 px-5 text-white font-semibold rounded transition-all duration-250 ease-in ${
+            page === 2
+              ? "bg-gray-500 hover:bg-gray-600 active:bg-gray-700"
+              : "bg-gray-400 hover:bg-gray-500 active:bg-gray-600"
+          }`}
+        >
+          2
+        </button>
+        <button
+          onClick={() => setPage(3)}
+          className={` py-3 px-5 text-white font-semibold rounded transition-all duration-250 ease-in ${
+            page === 3
+              ? "bg-gray-500 hover:bg-gray-600 active:bg-gray-700"
+              : "bg-gray-400 hover:bg-gray-500 active:bg-gray-600"
+          }`}
+        >
+          3
+        </button>
+        <button
+          onClick={() => setPage(4)}
+          className={` py-3 px-5 text-white font-semibold rounded transition-all duration-250 ease-in ${
+            page === 4
+              ? "bg-gray-500 hover:bg-gray-600 active:bg-gray-700"
+              : "bg-gray-400 hover:bg-gray-500 active:bg-gray-600"
+          }`}
+        >
+          4
+        </button>
+        <button
+          onClick={() => setPage(5)}
+          className={` py-3 px-5 text-white font-semibold rounded transition-all duration-250 ease-in ${
+            page === 5
+              ? "bg-gray-500 hover:bg-gray-600 active:bg-gray-700"
+              : "bg-gray-400 hover:bg-gray-500 active:bg-gray-600"
+          }`}
+        >
+          5
+        </button>
+      </div>
     </div>
   );
 }
