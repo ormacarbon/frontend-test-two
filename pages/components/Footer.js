4e23266d7d0c54183a52f211c76ef5eb2fc38c86
Emerson Araujo <emersonsantos@gmail.com>
2023-01-12 16:25:18 -0300

    feat: added style to navbar and footer

--- pages/components/Footer.js
+++ pages/components/Footer.js
@@ -1,7 +1,23 @@
+import styled from "styled-components";
+
+const Footer_style = styled.footer`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    color: #FFF;
+    background-color: #333;
+    height: 116px;
+    margin-top: 2em;
+    border-top: 2px  solid #FFBB11;
+`
+const Span = styled.span`
+    font-weight: bold;
+`
+
 export default function Footer() {
     return (
-       <footer>
-          <p><span>Pokémon</span>&copy; {new Date().getFullYear()}</p>
-       </footer>
+       <Footer_style>
+          <p><Span>Pokémon</Span> &copy; {new Date().getFullYear()}</p>
+       </Footer_style>
     );
  }; 
--- pages/components/Navbar.js
+++ pages/components/Navbar.js
@@ -3,35 +3,60 @@ import Image from 'next/image';
 
 import styled from 'styled-components';
 
- const Title = styled.h1`
-  color:    blue;
+const Container = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1em 1.2em;
+  margin-bottom: 2em;
+  background-color: #333;
+  color: #fff;
 `;
+const Logo = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+const Ul = styled.ul`
+    display: flex;
+    list-style: none;
+`
+const Li = styled.li`
+    margin-right: 1.3em;
+`
+const aa = styled.a``;
+const A = styled(aa)`
+    color: #fff;
+    cursor: pointer;
+    padding: 5px;
+    font-weight: bold;
+`
 
 
+  
 export default function Navbar() {
   return (
-    <nav>
-      <div>
+    <Container>
+      <Logo>
         <Image
           src='/images/pokemon.png'
           width='80'
           height='40'
           alt='Pokémon-logo'
         />
-        <Title>Pokémon</Title>
-      </div>
-      <ul>
-        <li>
+      </Logo>
+      <Ul>
+        <Li>
           <Link href='/' legacyBehavior>
-            <a>Home</a>
+            <A>Home</A>
           </Link>
-        </li>
-        <li>
+        </Li>
+        <Li>
           <Link href='/about' legacyBehavior>
-            <a>About</a>
+            <A>About</A>
           </Link>
-        </li>
-      </ul>
-    </nav>
+        </Li>
+      </Ul>
+    </Container>
   );
-};
+}
