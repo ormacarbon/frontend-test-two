69f735f42fb28ecfecfede28c7c0c6373c764bdd
Pedro L <pedrocontact22@gmail.com>
2023-01-12 00:40:46 -0300

    Add Tailwind CSS
    Tailwind CSS is much more reliable than Styled Components

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,13 @@
 import type { AppProps } from "next/app";
 import { QueryClientProvider } from "react-query";
+import Header from "../components/Header";
 import { queryClient } from "../services/queryClient";
+import "../styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
+      <Header />
       <Component {...pageProps} />;
     </QueryClientProvider>
   );
