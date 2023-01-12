e94bf1f0d705903a205869f20824b80f2d66088d
Elison Michell <dev.elisonmichell@gmail.com>
2023-01-12 18:35:01 -0300

    fix(layout): add overflow hidden to layout

--- components/layout/styles.js
+++ components/layout/styles.js
@@ -2,8 +2,11 @@ import styled from 'styled-components'
 
 export const Container = styled.section`
   display: flex;
+  height: 100vh;
+  overflow: hidden;
 
   main {
     flex: 1;
+    overflow-y: scroll;
   }
 `
