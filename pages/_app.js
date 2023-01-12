da6b04a6073879b860f752506301f2029c7f9781
= <f7lipe@me.com>
2023-01-12 14:04:15 -0300

    feat: Dark/Light mode
    - Create a global theme file that serves the Styled-components ThemeProvider.
    - An event listener is used to capture the preferred OS color scheme and then update the local state.

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,31 @@
+import { useState, useEffect } from "react"
+import { ThemeProvider } from "styled-components"
+import { lightTheme, darkTheme, GlobalStyles } from "../themeConfig"
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  const [theme, setTheme] = useState("light")
+
+  useEffect(() => {
+    window
+      .matchMedia('(prefers-color-scheme: dark)')
+      .addEventListener('change', event => {
+        setTheme(event.matches ? "dark" : "light")
+      })
+
+    return () => {
+      window
+        .matchMedia('(prefers-color-scheme: dark)')
+        .removeEventListener('change', () => { })
+    }
+  }, [])
+
+  return (
+    <ThemeProvider theme={theme == 'light' ? lightTheme : darkTheme}>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
 
 export default MyApp
