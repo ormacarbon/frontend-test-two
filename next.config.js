c55e15207bfa157c01ed680a7f467066617ff9a0
Elison Michell <dev.elisonmichell@gmail.com>
2023-01-12 18:39:56 -0300

    chore(next config): add poke api to image configured host

--- next.config.js
+++ next.config.js
@@ -2,6 +2,17 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'raw.githubusercontent.com',
+        port: '',
+        pathname: '/PokeAPI/sprites/master/sprites/**',
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
