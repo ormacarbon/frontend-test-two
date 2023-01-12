b4e36e485033c35e06de681e4cebd04d952f99e6
Geraldo Pereira da Costa Neto <80066270+GeracNeto@users.noreply.github.com>
2023-01-12 20:56:38 -0300

    styles: added ThemeProvider to the app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,13 @@
 import GlobalStyle from "../styles/global";
+import { ThemeProvider } from "styled-components";
+import theme from "../styles/theme";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <Component {...pageProps} />
       <GlobalStyle />
-    </>
+    </ThemeProvider>
   );
 }
 
