1407ab84cf1b03e77778e5b3a8a8964e859d248b
Elison Michell <dev.elisonmichell@gmail.com>
2023-01-13 00:38:17 -0300

    feat(overview): add new card components and pokemon contex to overview

--- pages/Overview/index.js
+++ pages/Overview/index.js
@@ -1,10 +1,17 @@
-import React from 'react'
-import Box from '../../components/Box';
-import VideoBanner from '../../components/VideoBanner';
-import { Container, Content } from './styles'
+import React, { useState, useContext } from "react";
+import Box from "../../components/Box";
+import VideoBanner from "../../components/VideoBanner";
+import { Container, Content } from "./styles";
+
+import { PokemonContext } from "../../context/pokemonContext";
+import SmallCard from "../../components/SmallCard";
+import ExpandedCard from "../../components/ExpandedCard";
 
 function Overview() {
-  return(
+  const [selectedPokemon, setSelectedPokemon] = useState(0);
+  const { pokemonTeam } = useContext(PokemonContext);
+
+  return (
     <Container>
       <header>
         <h2>Hi Pokémon trainer,</h2>
@@ -13,11 +20,41 @@ function Overview() {
       <VideoBanner />
       <Content>
         <Box title="Team of the day">
+          {pokemonTeam.length > 0 &&
+            pokemonTeam.map((pokemon, id) => {
+              return (
+                <SmallCard
+                  key={id}
+                  name={pokemon.name}
+                  active={selectedPokemon == id}
+                  sprite={
+                    pokemon.sprites.other["official-artwork"].front_default
+                  }
+                  id={pokemon.id}
+                  type={pokemon.types.map((tp) => tp.type.name)}
+                  onClick={() => setSelectedPokemon(id)}
+                />
+              );
+            })}
         </Box>
         <Box title="Pokémon quick view">
+          {pokemonTeam.length > 0 && (
+            <ExpandedCard
+              name={pokemonTeam[selectedPokemon].name}
+              sprite={
+                pokemonTeam[selectedPokemon].sprites.other["official-artwork"]
+                  .front_default
+              }
+              id={pokemonTeam[selectedPokemon].id}
+              type={pokemonTeam[selectedPokemon].types.map(
+                (tp) => tp.type.name
+              )}
+              description={pokemonTeam[selectedPokemon].flavor_text}
+            />
+          )}
         </Box>
       </Content>
     </Container>
-  )
+  );
 }
 export default Overview;
--- pages/Overview/styles.js
+++ pages/Overview/styles.js
@@ -3,11 +3,11 @@ import styled from 'styled-components'
 export const Container = styled.div`
   display: flex;
   flex-direction: column;
-  gap: ${props => props.theme.spacing.md};
-  padding: ${props => props.theme.spacing.xl}  ${props => props.theme.spacing['3xl']};
+  gap: ${props => props.theme.spacing.lg};
+  padding: ${props => props.theme.spacing['2xl']}  ${props => props.theme.spacing['3xl']};
   
   h1 {
-    font-size: ${props => props.theme.fonts.size['2xl']};
+    font-size: ${props => props.theme.fonts.size['3xl']};
   }
 
   h2 {
@@ -17,11 +17,8 @@ export const Container = styled.div`
 `
 
 export const Content = styled.div`
-  display: flex;
-  margin-top: ${props => props.theme.spacing.md};
-  gap: ${props => props.theme.spacing['2xl']};
-
-  & :nth-child(2) {
-    flex: 2;
-  }
+  display: grid;
+  grid-template-columns: 1fr 2fr;
+  margin-top: ${props => props.theme.spacing.lg};
+  gap: ${props => props.theme.spacing['3xl']};
 `
