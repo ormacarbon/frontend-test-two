e70ec069eb1481dd9a98054c0d86f4311f727a2d
Elison Michell <dev.elisonmichell@gmail.com>
2023-01-13 00:28:57 -0300

    fix(video banner): add missing color prop on button

--- components/VideoBanner/index.js
+++ components/VideoBanner/index.js
@@ -1,20 +1,20 @@
-import React from 'react'
-import Button from '../Button';
-import { Container, Content } from './styles'
+import React from "react";
+import Button from "../Button";
+import { Container, Content } from "./styles";
 
 function VideoBanner() {
-  return(
+  return (
     <Container>
       <Content>
         <h4>Pokémon Pokédex</h4>
         <p>Your definitive guide to the universe of Pokémons</p>
-        <Button title="Discover More"/>
+        <Button title="Explore" color="dark"/>
       </Content>
-      
+
       <video autoPlay loop muted>
         <source src="/pokemon-in-the-wild.mp4" type="video/mp4" />
       </video>
     </Container>
-  )
+  );
 }
 export default VideoBanner;
