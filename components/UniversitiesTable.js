824d0ebd92288a5401fa4f3728377cc1b278c7d6
Carolina Margiotti <55335180+CarolinaMargiotti@users.noreply.github.com>
2023-01-13 13:25:22 -0300

    stylized page

--- components/UniversitiesTable.js
+++ components/UniversitiesTable.js
@@ -1,33 +1,35 @@
-import { Table } from "../styles/componentStyles";
+import { Table, Tr, Th, Td, Overflow } from "../styles/componentStyles";
 
 export default function UniversitiesTable(props) {
 	const list = props?.list;
 
 	return (
-		<Table>
+		<div>
 			{list.length == 0 && <h2>No Results</h2>}
 			{list.length > 0 && (
-				<table>
-					<thead>
-						<tr>
-							<th>Name</th>
-							<th>Country</th>
-							<th>Webpage</th>
-							<th>Domain</th>
-						</tr>
-					</thead>
-					<tbody>
-						{list?.map((item, index) => (
-							<tr key={index}>
-								<td>{item?.name}</td>
-								<td>{item?.country}</td>
-								<td>{item?.web_pages[0]}</td>
-								<td>{item?.domains[0]}</td>
-							</tr>
-						))}
-					</tbody>
-				</table>
+				<Overflow>
+					<Table>
+						<thead>
+							<Tr>
+								<Th>Name</Th>
+								<Th>Country</Th>
+								<Th>Webpage</Th>
+								<Th>Domain</Th>
+							</Tr>
+						</thead>
+						<tbody>
+							{list?.map((item, index) => (
+								<Tr key={index}>
+									<Td>{item?.name}</Td>
+									<Td>{item?.country}</Td>
+									<Td>{item?.web_pages[0]}</Td>
+									<Td>{item?.domains[0]}</Td>
+								</Tr>
+							))}
+						</tbody>
+					</Table>
+				</Overflow>
 			)}
-		</Table>
+		</div>
 	);
 }
