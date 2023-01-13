390c703f6195d3225dbed85b4e69583920df0591
= <f7lipe@me.com>
2023-01-13 20:01:52 -0300

    Update next.config.js
    Bring environment variables to client side

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  env: {
+    API_BASE_URL: process.env.API_BASE_URL,
+    API_KEY: process.env.API_KEY
+  }
 }
 
 module.exports = nextConfig
