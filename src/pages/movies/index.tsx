a83f70f72b74e3574d36c869603e3d1e4387edd3
Geraldo Pereira da Costa Neto <80066270+GeracNeto@users.noreply.github.com>
2023-01-12 22:53:29 -0300

    front: updated 'movies' page

--- src/pages/movies/index.tsx
+++ src/pages/movies/index.tsx
@@ -1,3 +1,16 @@
+import AnotherEntertainment from "../../components/AnotherEntertainment";
+import Header from "../../components/Header";
+import TopEntertainment from "../../components/TopEntertainment";
+import { MainContent } from "../../styles/global";
+
 export default function Movies() {
-  return <h1>movies</h1>;
+  return (
+    <>
+      <Header />
+      <MainContent>
+        <AnotherEntertainment />
+        <TopEntertainment />
+      </MainContent>
+    </>
+  );
 }
