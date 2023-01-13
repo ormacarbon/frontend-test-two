dad752201fe74fe655166fa967a62ec9c807c45e
Miguel S. Barbosa <miguelbarbosacs@gmail.com>
2023-01-13 19:17:52 -0300

    [feat] Category list - add skeleton loader

--- src/components/layout/CategoriesList/CategoriesList.tsx
+++ src/components/layout/CategoriesList/CategoriesList.tsx
@@ -1,10 +1,11 @@
+import { CategoryListSkeleton } from "@/components/feedback";
 import { useListCategories } from "@/hooks/api/categories/useListCategories";
 import { removeDuplicates } from "@/src/helpers/removeDuplicates";
 import { CategoryTag } from "../CategoryTag/CategoryTag";
 import * as S from "./categories-list";
 
 export const CategoriesList = () => {
-  const { categoriesList: data } = useListCategories({
+  const { categoriesList: data, isLoading } = useListCategories({
     reqConfig: {
       params: {
         limit: 5,
@@ -18,9 +19,15 @@ export const CategoriesList = () => {
 
   return (
     <S.CategoryListContainer>
-      {categoriesList?.map((category) => (
-        <CategoryTag href={category} key={category} tagName={category} />
-      ))}
+      {isLoading ? (
+        <>
+          <CategoryListSkeleton />
+        </>
+      ) : (
+        categoriesList?.map((category) => (
+          <CategoryTag href={category} key={category} tagName={category} />
+        ))
+      )}
     </S.CategoryListContainer>
   );
 };
--- src/helpers/filterPostsByCategory.ts
+++ src/helpers/filterPostsByCategory.ts
@@ -9,11 +9,6 @@ export function filterPostsByCategory({
   categoryId,
   postsList,
 }: filterPostsByCategory) {
-  const categories = postsList?.map((post) => post.tags)?.flat();
-  const categoriesList = removeDuplicates(categories);
-
-  const category = categoriesList?.find((category) => category === categoryId);
-
   const postsByCategory = postsList?.filter((post) => {
     if (post.tags.find((tag) => tag === categoryId)) {
       return post;
