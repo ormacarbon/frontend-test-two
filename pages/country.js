539735ead0b18f4c9d24d4501458d5c051721630
Carolina Margiotti <55335180+CarolinaMargiotti@users.noreply.github.com>
2023-01-13 15:23:20 -0300

    Added titles to each page

--- pages/country.js
+++ pages/country.js
@@ -1,12 +1,16 @@
 import UniversitySearcher from "../components/UniversitySearcher";
 import { Context } from "../utils/AppContext";
 import { useContext } from "react";
+import Head from "next/head";
 
 export default function Country() {
 	const { searchUniversitiesByCountry } = useContext(Context);
 
 	return (
 		<div>
+			<Head>
+				<title>Country</title>
+			</Head>
 			<UniversitySearcher
 				dataRetrieverFunction={searchUniversitiesByCountry}
 				searchParamName="country"
