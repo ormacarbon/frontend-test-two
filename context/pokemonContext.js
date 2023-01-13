95828fa0f18a5137fb88f23ed83948eff4358e66
Elison Michell <dev.elisonmichell@gmail.com>
2023-01-13 12:06:08 -0300

    feat(context): add pokemon list request

--- context/pokemonContext.js
+++ context/pokemonContext.js
@@ -1,33 +1,58 @@
-import React, { createContext, useState, useEffect } from "react";
-import axios from "axios";
+import React, { createContext, useState, useEffect } from 'react'
+import axios from 'axios'
 
-export const PokemonContext = createContext(undefined);
+export const PokemonContext = createContext(undefined)
 
 export function PokemonContextProvider({ children }) {
-  const [pokemonTeam, setPokemonTeam] = useState([]);
+  const [pokemonTeam, setPokemonTeam] = useState([])
+  const [pokemonList, setPokemonList] = useState([])
+  const [limit] = useState(12)
 
   function getRandomNumber(max) {
-    return Math.floor(Math.random() * max) + 1;
+    return Math.floor(Math.random() * max) + 1
   }
 
-  async function fetchData(url) {
+  async function fetchData(url, params) {
     try {
-      const response = await axios.get(url);
-      return response.data;
-    } catch (error) {
-      console.error(error);
+      const response = await axios.get(url, params)
+      return response.data
     }
+    catch (error) {
+      console.error(error)
+    }
+  }
+
+  async function fetchPokemonList(limit, offset) {
+    const response = await fetchData(`https://pokeapi.co/api/v2/pokemon`, {
+      params: {
+        limit: limit,
+        offset: offset
+      }}
+    )
+    return response.results
+  }
+
+  async function fetchPokemonFromList(pokemonList) {
+    const promisseList = pokemonList.map((pokemon) => fetchData(pokemon.url))
+
+    const list = await Promise.all(promisseList)
+    return list
+  }
+
+  async function initPokemonList() {
+    const list = await fetchPokemonList(limit)
+    const pokemonList = await fetchPokemonFromList(list)
+    setPokemonList(pokemonList)
   }
 
   async function fetchPokemon(id) {
     try {
-      const pokemon = await fetchData(
-        `https://pokeapi.co/api/v2/pokemon/${id}`
-      );
-      const description = await fetchPokemonDescription(pokemon.id);
-      return { ...pokemon, ...description };
-    } catch (error) {
-      console.error(error);
+      const pokemon = await fetchData(`https://pokeapi.co/api/v2/pokemon/${id}`)
+      const description = await fetchPokemonDescription(pokemon.id)
+      return { ...pokemon, ...description }
+    }
+    catch (error) {
+      console.error(error)
     }
   }
 
@@ -35,46 +60,46 @@ export function PokemonContextProvider({ children }) {
     try {
       const response = await fetchData(
         `https://pokeapi.co/api/v2/pokemon-species/${id}`
-      );
-      const descriptionList = await response.flavor_text_entries;
-      const description = await getLastDescription(descriptionList);
-      return description;
-    } catch (error) {
-      console.error(error);
+      )
+      const descriptionList = await response.flavor_text_entries
+      const description = await getLastDescription(descriptionList)
+      return description
+    }
+    catch (error) {
+      console.error(error)
     }
   }
 
   function getLastDescription(descriptionList) {
     const filterList = descriptionList.filter(
       (desc) => desc.language.name === "en"
-    );
-    return filterList.pop();
+    )
+    return filterList.pop()
   }
 
   async function getPokemonTeam() {
-    const pokemonPromises = Array(6)
-      .fill()
-      .map(() => fetchPokemon(getRandomNumber(905)));
+    const pokemonPromises = Array(6).fill().map(() => fetchPokemon(getRandomNumber(905)))
 
-    const team = await Promise.all(pokemonPromises);
-    return team;
+    const team = await Promise.all(pokemonPromises)
+    return team
+  }
+
+  async function initPokemonTeam() {
+    const randomTeam = await getPokemonTeam()
+    setPokemonTeam(randomTeam)
   }
 
   useEffect(() => {
-    async function initPokemonTeam() {
-      const randomTeam = await getPokemonTeam();
-      setPokemonTeam(randomTeam);
-    }
-
     initPokemonTeam();
-  }, []);
+    initPokemonList();
+  }, [])
 
   useEffect(() => {
-    console.log(pokemonTeam);
-  }, [pokemonTeam]);
+    console.log(pokemonList)
+  }, [pokemonList])
 
   return (
-    <PokemonContext.Provider value={{ pokemonTeam }}>
+    <PokemonContext.Provider value={{ pokemonTeam, pokemonList }}>
       {children}
     </PokemonContext.Provider>
   );
