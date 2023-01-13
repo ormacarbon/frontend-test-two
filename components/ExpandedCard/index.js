d292a45b892c949cae46ab7a460758e3679d78c9
Elison Michell <dev.elisonmichell@gmail.com>
2023-01-13 00:36:50 -0300

    feat(components): add expanded card component

---
+++ components/ExpandedCard/index.js
@@ -0,0 +1,51 @@
+import React from "react";
+import {
+  Container,
+  Frame,
+  Content,
+  Header,
+  Types,
+  Description,
+} from "./styles";
+
+import TypeLabel from "../TypeLabel";
+import Image from "next/image";
+import Button from "../Button";
+
+function ExpandedCard(props) {
+  return (
+    <Container active={props.active} onClick={props.onClick}>
+      <Frame>
+        {props.sprite && (
+          <Image
+            src={props.sprite}
+            alt={props.name + " Sprite"}
+            unoptimized
+            width={360}
+            height={360}
+          />
+        )}
+      </Frame>
+      <Content>
+        <Header>
+          <div className="title-container">
+            <p>
+              {props.name} <span>Nº&nbsp;{props.id}</span>
+            </p>
+          </div>
+          <Types>
+            {props.type &&
+              props.type.map((type, id) => (
+                <TypeLabel key={type} type={props.type[id]} />
+              ))}
+          </Types>
+        </Header>
+        <Description>{props.description}</Description>
+        <div className="button-container">
+          <Button title="Discover More" />
+        </div>
+      </Content>
+    </Container>
+  );
+}
+export default ExpandedCard;
---
+++ components/ExpandedCard/styles.js
@@ -0,0 +1,82 @@
+import styled from "styled-components";
+
+export const Container = styled.button`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  gap: ${(props) => props.theme.spacing.sm};
+  padding: ${(props) => props.theme.spacing.md};
+  border-radius: ${(props) => props.theme.spacing.xl};
+  color: ${(props) => props.theme.colors.light};
+  border: 1px solid ${(props) => props.theme.colors.gray[400]};
+  transition: background 0.3s, border 0.3s;
+`;
+
+export const Frame = styled.div`
+  width: 100%;
+  border-radius: ${(props) => props.theme.spacing.md};
+  background-color: transparent;
+  background-image: repeating-linear-gradient(45deg, transparent, transparent 30px, ${(props) => props.theme.colors.gray[500]} 30px, ${(props) => props.theme.colors.gray[500]} 60px);
+  border: 1px solid ${(props) => props.theme.colors.gray[400]};
+  img {
+    width: 100%;
+    height: 100%;
+    max-width: 475px;
+    max-height: 475px;
+  }
+`;
+
+export const Header = styled.header`
+  min-height: 3.5rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  .title-container {
+    display: flex;
+    gap: ${(props) => props.theme.spacing.sm};
+    font-size: ${(props) => props.theme.fonts.size["2xl"]};
+    font-weight: 600;
+
+    p {
+      text-align: start;
+      text-transform: capitalize;
+    }
+
+    span {
+      color: ${(props) => props.theme.colors.gray[300]};
+      border-radius: ${(props) => props.theme.spacing.lg};
+    }
+  }
+`;
+
+export const Content = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: ${(props) => props.theme.spacing.sm};
+
+  .button-container {
+    flex: 1;
+    display: flex;
+    justify-content: flex-end;
+    align-items: flex-end;
+  }
+`;
+
+export const Types = styled.div`
+  display: flex;
+  height: 100%;
+  flex-direction: column;
+  margin-top: ${(props) => props.theme.spacing.md};
+  gap: ${(props) => props.theme.spacing.xs};
+`;
+
+export const Description = styled.div`
+  height: 3.75rem;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  text-align: start;
+  font-size: ${(props) => props.theme.fonts.size.md};
+`;
