de1b6e4870f0c51cf734f450593f88e59531b981
Carolina Margiotti <55335180+CarolinaMargiotti@users.noreply.github.com>
2023-01-13 13:41:38 -0300

    created content for index

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,21 @@
-import Head from "../components/Head";
-
 export default function Home() {
 	return (
 		<div>
 			<h1>Home</h1>
+			<p>Seach for universities by different criteria!</p>
+			<br />
+			<h3>Country</h3>
+			<p>Type the name of a country and filter the results.</p>
+			<h3>Name</h3>
+			<p>
+				Type the name of an university or a word and get the results of
+				universities' names that contain the word.
+			</p>
+			<h3>Domain</h3>
+			<p>
+				Search by the full domain of the university! (If you happen to
+				know it).
+			</p>
 		</div>
 	);
 }
