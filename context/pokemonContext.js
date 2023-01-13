88897816b4251ef0bfab39ab2fabf4d7b2d24c7a
Elison Michell <dev.elisonmichell@gmail.com>
2023-01-13 00:36:06 -0300

    feat(context): add fetch pokemon description request

--- context/pokemonContext.js
+++ context/pokemonContext.js
@@ -1,52 +1,81 @@
-import React, { createContext, useState, useEffect } from 'react'
-import axios from 'axios'
+import React, { createContext, useState, useEffect } from "react";
+import axios from "axios";
 
-export const PokemonContext = createContext(undefined)
+export const PokemonContext = createContext(undefined);
 
-export function PokemonContextProvider({children}) {
+export function PokemonContextProvider({ children }) {
   const [pokemonTeam, setPokemonTeam] = useState([]);
 
   function getRandomNumber(max) {
-    return Math.floor(Math.random() * max) + 1
+    return Math.floor(Math.random() * max) + 1;
   }
 
-  async function getRandomPokemon() {
-    try{
-      return await fetchPokemonById(getRandomNumber(905))
-    }
-    catch(error) {
-      console.error(error)
+  async function fetchData(url) {
+    try {
+      const response = await axios.get(url);
+      return response.data;
+    } catch (error) {
+      console.error(error);
     }
   }
 
-  async function fetchPokemonById(id) {
-    try{
-      const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`)
-      return response.data
+  async function fetchPokemon(id) {
+    try {
+      const pokemon = await fetchData(
+        `https://pokeapi.co/api/v2/pokemon/${id}`
+      );
+      const description = await fetchPokemonDescription(pokemon.id);
+      return { ...pokemon, ...description };
+    } catch (error) {
+      console.error(error);
     }
-    catch(error) {
-      console.error(error)
+  }
+
+  async function fetchPokemonDescription(id) {
+    try {
+      const response = await fetchData(
+        `https://pokeapi.co/api/v2/pokemon-species/${id}`
+      );
+      const descriptionList = await response.flavor_text_entries;
+      const description = await getLastDescription(descriptionList);
+      return description;
+    } catch (error) {
+      console.error(error);
     }
+  }
+
+  function getLastDescription(descriptionList) {
+    const filterList = descriptionList.filter(
+      (desc) => desc.language.name === "en"
+    );
+    return filterList.pop();
   }
 
   async function getPokemonTeam() {
-    const pokemonPromises = Array(6).fill().map(() => getRandomPokemon())
-    const team = await Promise.all(pokemonPromises)
-    return team
+    const pokemonPromises = Array(6)
+      .fill()
+      .map(() => fetchPokemon(getRandomNumber(905)));
+
+    const team = await Promise.all(pokemonPromises);
+    return team;
   }
 
   useEffect(() => {
     async function initPokemonTeam() {
-      const randomTeam = await getPokemonTeam()
-      setPokemonTeam(randomTeam)
+      const randomTeam = await getPokemonTeam();
+      setPokemonTeam(randomTeam);
     }
-    
-    initPokemonTeam()
-  },[])
+
+    initPokemonTeam();
+  }, []);
+
+  useEffect(() => {
+    console.log(pokemonTeam);
+  }, [pokemonTeam]);
 
   return (
-    <PokemonContext.Provider value={{pokemonTeam}}>
+    <PokemonContext.Provider value={{ pokemonTeam }}>
       {children}
     </PokemonContext.Provider>
-  )
+  );
 }
