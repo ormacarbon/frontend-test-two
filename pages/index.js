6a646a5b5ad0fe67632880d404219fc60ac159e5
Gustavo Nep <gustavo._12@hotmail.com>
2023-01-12 22:01:06 -0300

    feat: add button component to home

--- pages/index.js
+++ pages/index.js
@@ -1,44 +1,9 @@
 import Link from "next/link";
 import styled from "styled-components";
 import Navbar from "../components/navbar";
+import Button from "../components/button";
 
 
-const StyledButton = styled.button`
-  border-radius: 24px;
-  background-color: #fff;
-  margin-left: 100px;
-  appearance: none;
-  padding: 10px 30px;
-  transition: box-shadow 280ms cubic-bezier(.4, 0, .2, 1),opacity 15ms linear 30ms,transform 270ms cubic-bezier(0, 0, .2, 1) 0ms;
-  &:hover {
-    background: #F6F9FE;
-    color: #174ea6;
-  }
-  &:focus {
-    color: palevioletred;
-    outline: none;
-    border: 2px solid #4285f4;
-  }
-  &:active {
-    box-shadow: 0 4px 4px 0 rgb(60 64 67 / 30%), 0 8px 12px 6px rgb(60 64 67 / 15%);
-    outline: none;
-  }
-  &:not(:disabled) {
-  box-shadow: rgba(60, 64, 67, .3) 0 1px 3px 0, rgba(60, 64, 67, .15) 0 4px 8px 3px;
-}
-  &:not(:disabled):hover {
-  box-shadow: rgba(60, 64, 67, .3) 0 2px 3px 0, rgba(60, 64, 67, .15) 0 6px 10px 4px;
-}
-  &:not(:disabled):focus {
-  box-shadow: rgba(60, 64, 67, .3) 0 1px 3px 0, rgba(60, 64, 67, .15) 0 4px 8px 3px;
-}
-  &:not(:disabled):active {
-  box-shadow: rgba(60, 64, 67, .3) 0 4px 4px 0, rgba(60, 64, 67, .15) 0 8px 12px 6px;
-}
-  &:disabled {
-  box-shadow: rgba(60, 64, 67, .3) 0 1px 3px 0, rgba(60, 64, 67, .15) 0 4px 8px 3px;
-}
-`
 const CentralDiv = styled.div`
   height: 84.9vh;
   display: flex;
@@ -54,7 +19,7 @@ export default function Home() {
         <div>
           <h2>Welcome to the Cripto World</h2>
           <p>Click and enjoy our product</p>
-          <StyledButton>Click here</StyledButton>
+          <Button butname="Click Here"/>  
           <div></div>
         </div>
       </CentralDiv>
