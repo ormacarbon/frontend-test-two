5c5a033b750e7364284950a3b317ad7d1676aba7
Miguel S. Barbosa <miguelbarbosacs@gmail.com>
2023-01-13 19:18:00 -0300

    [feat] posts list - add skeleton loader

--- src/components/layout/PostsList/PostsList.tsx
+++ src/components/layout/PostsList/PostsList.tsx
@@ -1,9 +1,10 @@
+import { PostListSkeleton } from "@/components/feedback";
 import { useListPosts } from "@/hooks/api/posts/useListPosts";
 import { PostCard } from "../PostCard/PostCard";
 import * as S from "./posts-list.style";
 
 export const PostsList = () => {
-  const { postsList } = useListPosts({
+  const { postsList, isLoading } = useListPosts({
     reqConfig: {
       params: {
         limit: 5,
@@ -13,9 +14,11 @@ export const PostsList = () => {
 
   return (
     <S.PostsListContainer>
-      {postsList?.posts.map((post) => (
-        <PostCard key={post.id} post={post} />
-      ))}
+      {isLoading ? (
+        <PostListSkeleton />
+      ) : (
+        postsList?.posts.map((post) => <PostCard key={post.id} post={post} />)
+      )}
     </S.PostsListContainer>
   );
 };
