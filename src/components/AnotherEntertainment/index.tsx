4a7d04d8276d1ed7934589e5e5b405cd74f292fd
Geraldo Pereira da Costa Neto <80066270+GeracNeto@users.noreply.github.com>
2023-01-12 22:52:33 -0300

    front: created 'AnotherEntertaiment' and 'TopEntertainment' pages

---
+++ src/components/AnotherEntertainment/index.tsx
@@ -0,0 +1,12 @@
+export default function AnotherEntertainment() {
+  return (
+    <main>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestiae et,
+        accusamus libero sint quasi ex enim, quod quisquam ratione pariatur,
+        corporis quidem nam ipsam. Ratione earum sapiente ipsam harum
+        distinctio!
+      </p>
+    </main>
+  );
+}
---
+++ src/components/TopEntertainment/index.tsx
@@ -0,0 +1,18 @@
+import { AsideTop } from "./styles";
+
+export default function TopEntertainment() {
+  return (
+    <AsideTop>
+      <img
+        src="https://m.media-amazon.com/images/M/MV5BNzk1OGU2NmMtNTdhZC00NjdlLWE5YTMtZTQ0MGExZTQzOGQyXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_.jpg"
+        alt="top"
+      />
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestiae et,
+        accusamus libero sint quasi ex enim, quod quisquam ratione pariatur,
+        corporis quidem nam ipsam. Ratione earum sapiente ipsam harum
+        distinctio!
+      </p>
+    </AsideTop>
+  );
+}
