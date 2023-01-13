c96ba450a89e1e62127f250e210858a24869f8c4
Geraldo Pereira da Costa Neto <80066270+GeracNeto@users.noreply.github.com>
2023-01-12 22:53:01 -0300

    styles: created styles for 'TopEnterntaiment' page

---
+++ src/components/TopEntertainment/styles.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const AsideTop = styled.aside`
+  width: 100%;
+
+  img {
+    max-width: 100%;
+    height: auto;
+    object-fit: contain;
+  }
+`;
