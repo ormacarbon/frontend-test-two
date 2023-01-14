a751ec2afa132bc2594a24548d3105fcf74ece8f
FOXXX <75998678+devrodrigomolina@users.noreply.github.com>
2023-01-14 02:51:55 -0300

    feat: Criado estilos globais

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,12 @@
 import Header from "../components/Header"
+import { GlobalStyles } from "../styles/GlobalStyle"
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Header />
       <Component {...pageProps} />
+      <GlobalStyles />
     </>
   )
 }
---
+++ src/styles/GlobalStyle.js
@@ -0,0 +1,22 @@
+import styled, { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+ *, ::after, ::before {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  a {
+    text-decoration: none;
+  }
+  ul {
+    list-style: none;
+  }
+  body {
+    font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+  }
+  img {
+    width: 100%;
+    display: block;
+  }
+`
