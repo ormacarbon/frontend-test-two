cce3775ccb1aede6c8e0e8f69aec67e2ace2c4a2
FOXXX <75998678+devrodrigomolina@users.noreply.github.com>
2023-01-14 19:39:00 -0300

    feat: Criado contexto de temas

---
+++ src/contexts/themeContext.js
@@ -0,0 +1,27 @@
+import { createContext, useEffect, useState } from "react";
+
+export const ThemeContext = createContext();
+
+export const darkTheme = {
+  background: 'black',
+  text: 'white',
+}
+export const lightTheme = {
+  background: 'white',
+  text: 'black',
+}
+
+
+const ThemeProvider = (props) => {
+  const [theme, setTheme] = useState(lightTheme)
+  if(theme === 'dark') {
+    setTheme(darkTheme)
+  }
+  return (
+    <ThemeContext.Provider value={{theme, setTheme}}>
+      {props.children}
+    </ThemeContext.Provider>
+  );
+};
+
+export default ThemeProvider;
