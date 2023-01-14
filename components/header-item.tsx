2c453b1e91a4c00dc1a5311fb78887612193c325
Elesiann <giovanimcorrea18@gmail.com>
2023-01-14 19:12:25 -0300

    dark/light mode tweaks

--- components/header-item.tsx
+++ components/header-item.tsx
@@ -2,7 +2,6 @@ import Link from 'next/link';
 import { useRouter } from 'next/router';
 import styled from "styled-components";
 
-
 const HeaderItem = (props) => {
   const { title, href } = props
   const route = useRouter()
--- pages/_app.js
+++ pages/_app.js
@@ -5,17 +5,20 @@ import { applicationContext } from '../context/context';
 
 function MyApp({ Component, pageProps }) {
   const [theme, setTheme] = useState('dark-theme');
-  const [width, setWidth] = useState(0);
   const apiKey = '43090d0ed080a422f191b4b3db131431'
 
   const toggleTheme = () => {
     setTheme(theme == "white-theme" ? "dark-theme" : "white-theme");
   }
+  useEffect(() => {
+    localStorage.setItem('theme', theme)
+  }, []);
 
   useEffect(() => {
     document.body.classList.toggle('dark-theme')
-    document.body.classList.add('white-theme')
+    // document.body.classList.add('white-theme')
   }, [theme]);
+
 
   return (
     <applicationContext.Provider value={{ apiKey, theme }}>
