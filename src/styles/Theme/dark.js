a1f478f33125709fb10f6cb28ee0e73bd7d62003
Diêgo Raian <diegofrr.19@gmail.com>
2023-01-14 05:22:27 -0300

    [feat] Adição das propriedades dos temas

--- src/styles/Theme/dark.js
+++ src/styles/Theme/dark.js
@@ -1,8 +1,41 @@
 export const dark = {
   name: "dark",
   colors: {
-    primary: "#000000",
-    secundary: "#fade55",
-    bgColor: "orange",
+    primary: "#FFFFFF",
+    secundary: "#677483",
+
+    bg: "#171C24",
+    blue: "#5484FF",
+    red: "#FF3838",
+
+    button: {
+      bg: "#262D38",
+      text: "#8B99AA",
+    },
+
+    input: {
+      bg: "#101419",
+      placeholder: "#677483",
+      text: "#FFFFFF",
+    },
+
+    issue: {
+      user: "#8D9FB5",
+      title: "#FFFFFF",
+    },
+
+    dropdown: {
+      bg: "#262D38",
+      text: "#8D9FB5",
+      divider: "#3c424c",
+      shadow: "#000000",
+    },
+
+    header: {
+      title: "#C0C9D2",
+      border: "#2e333a",
+    },
+
+    imgBackground: "#1e232b",
   },
 };
--- src/styles/Theme/light.js
+++ src/styles/Theme/light.js
@@ -1,8 +1,42 @@
 export const light = {
   name: "light",
   colors: {
-    primary: "#FFFFFF",
-    secundary: "#ff0000",
-    bgColor: "blue",
+    primary: "#171C24",
+    secundary: "#677483",
+
+    bg: "#FFFFFF",
+    blue: "#5484FF",
+    red: "#FF3838",
+
+    button: {
+      bg: "#F0F1F3",
+      text: "#677483",
+    },
+
+    input: {
+      bg: "#FAFAFA",
+      placeholder: "#677483",
+      text: "#171C24",
+    },
+
+    issue: {
+      header_title: "#171C24",
+      user: "#677483",
+      title: "#171C24",
+    },
+
+    dropdown: {
+      bg: "#FFFFFF",
+      text: "#677483",
+      divider: "#e6e6e6",
+      shadow: "#677483",
+    },
+
+    header: {
+      title: "#171C24",
+      border: "#e6e6e6",
+    },
+
+    imgBackground: "#f7f7f7",
   },
 };
