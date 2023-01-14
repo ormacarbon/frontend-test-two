1e628bc1f78aa80a0eb223f7408757bd7915c060
Elison Michell <dev.elisonmichell@gmail.com>
2023-01-13 23:49:31 -0300

    feat(input)!: add input with filter based on pokemon name

--- components/Input/index.js
+++ components/Input/index.js
@@ -1,14 +1,29 @@
-import React from 'react'
+import React, { useContext, useCallback } from 'react'
 import { Container } from './styles'
 import { MagnifyingGlass } from 'phosphor-react';
 import Button from '../Button'
+import {DatalistInput, startsWithValueFilter } from 'react-datalist-input';
+import { PokemonContext } from '../../context/pokemonContext'
 
 function Input() {
+  const { referenceList } = useContext(PokemonContext);
+  const limitOptionsFilter = useCallback((items) => items.slice(0, 5), []);
+  const closeMatchOptionsFilter = useCallback((items, value) => items.filter((item) => item.value !== value.toLowerCase()),[]);
+  const filters = [startsWithValueFilter, limitOptionsFilter, closeMatchOptionsFilter];
+
   return(
     <Container>
-      <input type={'text'} placeholder={'Search Pokémon'}/>
-      <Button color="transparent" hoverColor="primary200" icon={<MagnifyingGlass size={20} weight="bold"/>}/>
-      
+      <Button color="primary200" hoverColor="primary300" icon={<MagnifyingGlass size={20} weight="bold"/>}/>
+      {
+        referenceList.length > 0 &&
+        <DatalistInput
+          placeholder='Search Pokémon'
+          onSelect={(item) => console.log(item.value)}
+          items={referenceList.map(item => {return({id:item.name, value:item.name})})}
+          highlightProps={{as: 'span'}}
+          filters={filters}
+        />
+      }
     </Container>
   )
 }
--- components/Input/styles.js
+++ components/Input/styles.js
@@ -1,20 +1,53 @@
 import styled from 'styled-components'
 
-export const Container = styled.label`
+export const Container = styled.div`
   display: flex;
-  flex-direction: row-reverse;
   align-items: center;
-  gap: ${(props) => props.theme.spacing.lg};
-  border: 1px solid ${(props) => props.theme.colors.gray400};
-  border-radius: ${(props) => props.theme.spacing['3xl']};
-  padding: ${(props) => props.theme.spacing.sm};
+  gap: ${(props) => props.theme.spacing.sm};
 
-  input {
+  .react-datalist-input__container {
+    position: relative;
+  }
+
+  .react-datalist-input__textbox {
     background: transparent;
     font-weight: 500;
-    min-width: 15rem;
+    min-width: 20rem;
     font-size: ${(props) => props.theme.fonts.size.md};
     color: ${(props) => props.theme.colors.light};
+    padding: ${(props) => props.theme.spacing.md};
+    border-radius: ${(props) => props.theme.spacing.md};
+    border: 1px solid ${(props) => props.theme.colors.gray400};
     outline: none;
+  }
+
+  .react-datalist-input__listbox {
+    position: absolute;
+    display: flex;
+    width: 100%;
+    margin: ${(props) => props.theme.spacing.xs} 0 0 0;
+    border: 1px solid ${(props) => props.theme.colors.gray400};
+    border-radius: ${(props) => props.theme.spacing.md};
+    background-color: ${(props) => props.theme.colors.dark};
+    overflow: hidden;
+    flex-direction: column;
+    list-style-type: none;
+    z-index: 1;
+  }
+
+  .react-datalist-input__listbox-option {
+    cursor: pointer;
+    padding: ${(props) => props.theme.spacing.lg};
+    &::selection {
+      background: red;
+    }
+  }
+
+  .react-datalist-input__listbox-option:focus {
+    background-color: ${(props) => props.theme.colors.primary200};
+  }
+
+  .react-datalist-input__listbox-option:hover {
+    background-color: ${(props) => props.theme.colors.primary200};
   }
 `
