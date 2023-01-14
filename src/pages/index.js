74d15efa136ccc03bed51e52e7a0337e2680418c
FOXXX <75998678+devrodrigomolina@users.noreply.github.com>
2023-01-14 19:36:55 -0300

    feat: Adicionado componente cards e passando informaçoes pra ele via prop

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,43 @@
+import { useContext } from "react";
 import Banner from "../components/Banner/Banner";
+import Cards from "../components/Cards/Cards";
+import Loading from "../components/Loading/Loading";
+import { ThemeContext } from "../contexts/themeContext";
 import { useFetch } from "../hooks/useFetch";
 import useLocation from "../hooks/useLocation";
+import * as C from './style'
 
 const Index = () => {
- 
-  const { latitude, longitude } = useLocation()
-  const { data: weather, error, loading } = useFetch(`/api/weather?latitude=${latitude}&longitude=${longitude}`)
+  const { theme } = useContext(ThemeContext)
+
+
+  const { latitude, longitude } = useLocation();
+
+  const {
+    data: weather,
+    error,
+    loading,
+  } = useFetch(`/api/weather?latitude=${latitude}&longitude=${longitude}`);
+  const { data } = useFetch(
+    `/api/forecast?latitude=${latitude}&longitude=${longitude}`
+  );
 
   return (
-    <div>
-      <Banner data={weather}/>
-      <code>
-        <pre>{JSON.stringify(weather, null, 2)}</pre>
-      </code>
-    </div>
-  )
-}
+    <C.ContainerHome style={{background: theme.background}}>
+      {loading ? (
+        <Loading />
+      ) : (
+        <>
+        <Banner data={weather} />
+        <C.Cards>
+          {data?.list.map((forecast) => (
+            <Cards forecast={forecast} />
+          ))}
+        </C.Cards>
+      </>
+      )}
+    </C.ContainerHome>
+  );
+};
 
-export default Index
+export default Index;
