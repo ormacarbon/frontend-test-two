4c4c253dd42e40030be18f12a416c134bb6dffe8
FOXXX <75998678+devrodrigomolina@users.noreply.github.com>
2023-01-14 20:42:43 -0300

    refactor: Aficionado classe no componente

--- src/components/Loading/Loading.js
+++ src/components/Loading/Loading.js
@@ -1,8 +1,10 @@
 import * as C from './style'
+import { AiOutlineLoading3Quarters } from 'react-icons/ai'
 
 const Loading = () => {
   return (
     <C.ContainerLoading>
+      <AiOutlineLoading3Quarters className='icon' />
       <h4>Carregando ...</h4>
     </C.ContainerLoading>
   )
