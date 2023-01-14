e27e01e9b6d8e1bcfb9ab2b5ff8a884335791b24
FOXXX <75998678+devrodrigomolina@users.noreply.github.com>
2023-01-14 19:35:37 -0300

    style: Criado estilização da pagina principal

---
+++ src/pages/style.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const ContainerHome = styled.div`
+  padding-top: 70px;
+`
+export const Cards = styled.div`
+  width: 100%;
+  max-width: 95vw;
+  margin: 50px auto;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 20px;
+  flex-wrap: wrap;
+`
