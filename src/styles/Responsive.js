235f3876e73a15ffed4f0f5ef71bfed1413ee0bf
FOXXX <75998678+devrodrigomolina@users.noreply.github.com>
2023-01-14 19:35:58 -0300

    feat: Criado arquivo de tratamento de responsividade

---
+++ src/styles/Responsive.js
@@ -0,0 +1,19 @@
+export const sizes = {
+  mobileS: '320px',
+  mobileM: '375px',
+  mobileL: '425px',
+  tablet: '760px',
+  laptop: '1024px',
+  laptopL: '1440px',
+  desktop: '2560px',
+};
+
+export const devices = {
+  mobileS: `(max-width: ${sizes.mobileS})`,
+  mobileM: `(max-width: ${sizes.mobileM})`,
+  mobileL: `(max-width: ${sizes.mobileL})`,
+  tablet: `(max-width: ${sizes.tablet})`,
+  laptop: `(max-width: ${sizes.laptop})`,
+  laptopL: `(max-width: ${sizes.laptopL})`,
+  desktop: `(max-width: ${sizes.desktop})`,
+};
