d489c7e0efc8dee15e5106472f19f91e71b5b119
FOXXX <75998678+devrodrigomolina@users.noreply.github.com>
2023-01-14 19:33:48 -0300

    feat: Adicionado informaçoes sobre o vento

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,18 +1,17 @@
 import * as C from './style'
-import bannerImage from '../../assets/banner-img.png'
 import Image from 'next/image'
-
 
 const Banner = ({ data }) => {
   const date = new Date()
+
   return (
     <C.ContainerBanner>
-      <Image className='bannerImage' src={bannerImage} />
+  
       <C.InfosBanner>
         <div className='container-infos'>
           {data?.weather.map(weather => (
             <div key={weather.id} className='weather-infos'>
-              <h1>{weather.main}</h1>
+              <h1>{data?.name}</h1>
               <h1>{weather.description}</h1>
               <p>{date.toDateString()}</p>
             </div>
@@ -24,6 +23,13 @@ const Banner = ({ data }) => {
             <p>Max: {data?.main?.temp_max}</p>
           </div>
       </C.InfosBanner>
+      <C.InfosTemp>
+          <h3>Wind Speed: {data?.wind?.speed}</h3>
+          <h3>Wind Deg: {data?.wind?.deg}</h3>
+          <h3>Wind Gust: {data?.wind?.gust}</h3>
+          
+      </C.InfosTemp>
+      
     </C.ContainerBanner>
   )
 }
