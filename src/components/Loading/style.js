f3ff6cb47eecdca591c877de4cc54ec9f3c98145
FOXXX <75998678+devrodrigomolina@users.noreply.github.com>
2023-01-14 20:43:01 -0300

    feat: Adicionado animação no loading

--- src/components/Loading/style.js
+++ src/components/Loading/style.js
@@ -1,6 +1,23 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
+const rotateLoading = keyframes`
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(359deg);
+  }
+`
 export const ContainerLoading = styled.div`
+  width: 100%;
+  height: 100vh;
   text-align: center;
   margin-top: 80px;
+  color: #551A8B;
+  .icon {
+    font-size: 3rem;
+    animation: ${rotateLoading} 2s infinite linear;
+  }
+
+  
 `
