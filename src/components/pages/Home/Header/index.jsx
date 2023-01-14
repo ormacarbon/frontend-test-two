922cf0b65ab64bbf77cadfbd458cc8d7498f47b1
Diêgo Raian <diegofrr.19@gmail.com>
2023-01-14 20:43:43 -0300

    [refactor] Refatoração no código do componente Header

--- src/components/pages/Home/Header/index.jsx
+++ src/components/pages/Home/Header/index.jsx
@@ -1,21 +1,11 @@
 import Link from "next/link";
+import ToggleThemeButton from "../../../shared/ToggleThemeButton";
+import Button from "../../../shared/Button";
 
-import { useContext } from "react";
-import { FiSun, FiMoon, FiArrowLeft } from "react-icons/fi";
-import {
-  AppName,
-  ButtonsContainer,
-  Container,
-  Content,
-  ToggleThemeButton,
-} from "./styles";
-import { ThemeContext } from "../../../contexts/theme";
+import { AppName, ButtonsContainer, Container, Content } from "./styles";
 import { useRouter } from "next/router";
 
-import Button from "../Button";
-
 export default function Header() {
-  const { theme, toggleTheme } = useContext(ThemeContext);
   const router = useRouter();
 
   const goToBack = () => router.back();
@@ -30,9 +20,7 @@ export default function Header() {
             y<span>ssues</span>
           </AppName>
           <ButtonsContainer>
-            <ToggleThemeButton onClick={toggleTheme}>
-              {theme.name === "light" ? <FiMoon /> : <FiSun />}
-            </ToggleThemeButton>
+            <ToggleThemeButton />
             <Button primary type="rounded" onClick={goToFavorites}>
               Favoritos
             </Button>
--- src/components/pages/Home/Header/styles.js
+++ src/components/pages/Home/Header/styles.js
@@ -35,28 +35,6 @@ export const AppName = styled.div`
   }
 `;
 
-export const ToggleThemeButton = styled.button`
-  width: 32px;
-  height: 32px;
-  display: grid;
-  place-items: center;
-  background-color: transparent;
-  border-radius: 6px;
-
-  svg {
-    transition: all 0.3s ease;
-    color: ${(p) => p.theme.colors.button.text};
-    width: 20px;
-    height: 20px;
-  }
-
-  :hover {
-    svg {
-      color: ${(p) => p.theme.colors.primary};
-    }
-  }
-`;
-
 export const ButtonsContainer = styled.div`
   display: flex;
   align-items: center;
