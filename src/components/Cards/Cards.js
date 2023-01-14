9728ddd906874853c27d1054b837fc359fc7f535
FOXXX <75998678+devrodrigomolina@users.noreply.github.com>
2023-01-14 20:41:40 -0300

    feat: Adicionado mais informações

--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -10,6 +10,15 @@ const Cards = ({forecast}) => {
         <div key={id}>
           <p>{description}</p>
           <p>{main}</p>
+          <p>Feels Like: {forecast.main.feels_like}</p>
+          <p>grnd level: {forecast.main.grnd_level}</p>
+          <p>Humidity: {forecast.main.humidity}</p>
+          <p>Pressure: {forecast.main.pressure}</p>
+          <p>Sea level: {forecast.main.sea_level}</p>
+          <p>Temp: {forecast.main.temp}</p>
+          <p>Temp kf: {forecast.main.temp_kf}</p>
+          <p>Temp MAX: {forecast.main.temp_max}</p>
+          <p>Temp MIN: {forecast.main.temp_min}</p>
         </div>
       ))}
 
