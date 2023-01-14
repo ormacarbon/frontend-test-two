c0f6a593936851967880425bf381c5c020cb9d5f
FOXXX <75998678+devrodrigomolina@users.noreply.github.com>
2023-01-14 19:39:11 -0300

    feat: Criado componente de loading

---
+++ src/components/Loading/Loading.js
@@ -0,0 +1,11 @@
+import * as C from './style'
+
+const Loading = () => {
+  return (
+    <C.ContainerLoading>
+      <h4>Carregando ...</h4>
+    </C.ContainerLoading>
+  )
+}
+
+export default Loading
