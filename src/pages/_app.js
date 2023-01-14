e4ce4450698462afca885a9c0d02302a7f1b6189
FOXXX <75998678+devrodrigomolina@users.noreply.github.com>
2023-01-14 19:38:36 -0300

    feat: Envolvendo o APP com o context de temas

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,13 +1,18 @@
 import Header from "../components/Header/Header"
+import ThemeProvider from "../contexts/themeContext"
 import { GlobalStyles } from "../styles/GlobalStyle"
 
 function MyApp({ Component, pageProps }) {
+
   return (
     <>
+    <ThemeProvider>
       <Header />
       <Component {...pageProps} />
       <GlobalStyles />
+    </ThemeProvider>
     </>
+
   )
 }
 
