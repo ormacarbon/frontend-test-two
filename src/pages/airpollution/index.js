c59242193060512d20a8cb562826bbe263f37dc2
FOXXX <75998678+devrodrigomolina@users.noreply.github.com>
2023-01-14 19:38:02 -0300

    feat: Feito map nos dados da api e mostrando o resultado

--- src/pages/airpollution/index.js
+++ src/pages/airpollution/index.js
@@ -1,24 +1,28 @@
 import { useEffect, useState } from "react";
 import { useFetch } from "../../hooks/useFetch";
+import * as C from './style'
+import useLocation from '../../hooks/useLocation'
 
 const AirPolluition = () => {
-  const [coords, setCoords] = useState(null);
-  
-  const { data, error, loading } = useFetch(`/api/air_pollution?latitude=${coords?.latitude}&longitude=${coords?.longitude}`)
+  const { latitude, longitude } = useLocation();
+  const { data, error, loading } = useFetch(`/api/air_pollution?latitude=${latitude}&longitude=${longitude}`)
 
-  useEffect(() => {
-    navigator.geolocation.getCurrentPosition((position) => {
-      const { latitude, longitude } = position.coords
-      setCoords({ latitude, longitude })
-    });
-  }, [])
 
   return (
-    <div>
-      <code>
-        <pre>{JSON.stringify(data, null, 2)}</pre>
-      </code>
-    </div>
+    <C.ContainerAirPolluition>
+      {data?.list?.map(elements => (
+        <>
+          <h1>CO: {elements.components.co}</h1>
+          <h1>NH3: {elements.components.nh3}</h1>
+          <h1>NO: {elements.components.no}</h1>
+          <h1>NO2: {elements.components.no2}</h1>
+          <h1>O3: {elements.components.o3}</h1>
+          <h1>pm2_5: {elements.components.pm2_5}</h1>
+          <h1>pm10: {elements.components.pm10}</h1>
+          <h1>so2: {elements.components.so2}</h1>
+        </>
+      ))}
+    </C.ContainerAirPolluition>
   )
 }
 
