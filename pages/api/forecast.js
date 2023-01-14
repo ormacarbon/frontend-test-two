679cc9a894997055053056f8a3a3b6af9f0c1456
FOXXX <75998678+devrodrigomolina@users.noreply.github.com>
2023-01-14 02:33:17 -0300

    feat: Criado api de proteção de key da requisição de previsão do tempo

---
+++ pages/api/forecast.js
@@ -0,0 +1,16 @@
+import axios from "axios"
+
+export default async function handler(req, res) {
+  const { latitude, longitude, ...query } = req.query
+
+  const response = await axios(`http://api.openweathermap.org/data/2.5/forecast`, {
+    params: {
+      lat: latitude,
+      lon: longitude,
+      appid: process.env.OPENWEATHERMAP_APPID,
+      ...query
+    }
+  })
+
+  res.status(response.status).json(response.data)
+}
