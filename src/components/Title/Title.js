e1e9bedcf0f5726f9715986f51bc975afe10d74d
FOXXX <75998678+devrodrigomolina@users.noreply.github.com>
2023-01-15 20:40:38 -0300

    feat: Criado componente de titulo

---
+++ src/components/Title/Title.js
@@ -0,0 +1,14 @@
+import React, { useContext } from 'react'
+import * as C from './style'
+import { ThemeContext } from '../../contexts/themeContext'
+
+const Title = ({ text }) => {
+  const { theme } = useContext(ThemeContext)
+  return (
+    <C.ContainerTitle theme={theme}>
+      <h2>{text}</h2>
+    </C.ContainerTitle>
+  )
+}
+
+export default Title
