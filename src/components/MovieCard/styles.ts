757ede6a7399f7b25eb84f7bafa88d9f028c87d7
Ivan Gomes <ivanjunior.pt@gmail.com>
2023-01-15 08:29:38 -0300

    Formatting data and modifying movie card styling

--- src/components/MovieCard/styles.ts
+++ src/components/MovieCard/styles.ts
@@ -1,27 +1,57 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
+import media from 'styled-media-query'
 
 export const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  align-items: center;
+  ${() => css`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
 
-  border-radius: 4px;
-  width: 12rem;
-  height: auto;
+    width: 15.2rem;
+    height: auto;
+
+    img {
+      border-radius: 4px;
+    }
+
+    ${media.lessThan('small')`
+      width: 11.5rem;
+    `}
+  `}
 `
 export const Content = styled.div`
-  display: flex;
-  flex-direction: column;
-  width: 100%;
-  padding: 5px;
+  ${() => css`
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    padding: 5px;
+
+    ${media.between('medium', 'large')`
+      padding: 0;
+      width: 70%;
+  `}
+  `}
 `
-export const Title = styled.h4`
-  color: ${(props) => props.theme['gray-900']};
-  font-weight: 700;
+export const Title = styled.h3`
+  ${({ theme }) => css`
+    color: ${theme.title};
+    font-weight: 700;
+
+    ${media.lessThan('small')`
+      font-size: 0.9rem;
+    `}
+  `}
 `
 export const Date = styled.p`
-  color: ${(props) => props.theme['gray-300']};
-  font-weight: 700;
-  font-size: 0.8rem;
+  ${({ theme }) => css`
+    color: ${theme.subtitle};
+    font-weight: 700;
+    font-size: 1rem;
+    margin-top: 5px;
+  `}
+
+  ${media.lessThan('small')`
+      font-size: 0.8rem;
+    `}
 `
