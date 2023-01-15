0e29997e95bf7bcf9a7a6219352ed6257874d2c7
FOXXX <75998678+devrodrigomolina@users.noreply.github.com>
2023-01-15 20:42:02 -0300

    style: Tratamento da responsividade

--- src/pages/airpollution/style.js
+++ src/pages/airpollution/style.js
@@ -1,8 +1,16 @@
 import styled from "styled-components";
+import { devices } from "../../styles/Responsive";
 
 export const ContainerAirPolluition = styled.div`
-  width: 100%;
-  max-width: 90vw;
+  width: 100vw;
   margin: 40px auto;
-
+  .infosPolluition {
+    max-width: 80vw;
+    margin: 140px auto;
+    display: flex;
+    justify-content: space-around;
+    @media ${devices.laptop} {
+      flex-direction: column;
+    }
+  }
 `
