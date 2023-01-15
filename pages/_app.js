3e15cca69aeb9c47f9875ab01c3ee9d06a2f5f26
costalucs <lucasdeslz@gmail.com>
2023-01-15 00:54:16 -0300

    feat: fetcher function

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,19 @@
+import { ThemeProvider } from 'styled-components'
+import Header from '../components/Header'
+import { PokemonProvider } from '../context/context'
+import Global from '../styles/global'
+import { main } from '../styles/main'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={main}>
+      <PokemonProvider>
+        <Global />
+        <Header title={'Pokedex'} />
+        <Component {...pageProps} />
+      </PokemonProvider>
+    </ThemeProvider>
+  )
 }
 
 export default MyApp
