187f250f2314b0b9a02d6246e2149cafe43f75cd
Elison Michell <dev.elisonmichell@gmail.com>
2023-01-15 16:48:51 -0300

    fix(frame): fix an issue where the image does not center horizontally

--- components/Frame/styles.js
+++ components/Frame/styles.js
@@ -3,14 +3,14 @@ import styled from 'styled-components'
 export const Container = styled.div`
   display: flex;
   justify-content: center;
+  align-items: center;
   width: 100%;
   border-radius: ${(props) => props.theme.spacing.md};
   background: ${(props) => props.theme.colors.gray500};
   border: 1px solid ${(props) => props.theme.colors.gray400};
   img {
     width: 100%;
-    height: 100%;
+    height: auto;
     max-width: 475px;
-    max-height: 475px;
   }
 `
