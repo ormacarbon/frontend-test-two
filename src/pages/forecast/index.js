78c580488a60b2d24877c95dcc0a37b0c8608921
FOXXX <75998678+devrodrigomolina@users.noreply.github.com>
2023-01-15 20:44:37 -0300

    refactor: Adicionado loading

--- src/pages/forecast/index.js
+++ src/pages/forecast/index.js
@@ -1,17 +1,24 @@
-import { useFetch } from '../../hooks/useFetch'
-import useLocation from '../../hooks/useLocation'
+import Loading from "../../components/Loading/Loading";
+import { useFetch } from "../../hooks/useFetch";
+import useLocation from "../../hooks/useLocation";
 
 const index = () => {
-  const { latitude, longitude } = useLocation()
-  const { data, error, loading } = useFetch(`/api/forecast?latitude=${latitude}&longitude=${longitude}`)
+  const { latitude, longitude } = useLocation();
+  const { data, error, loading } = useFetch(
+    `/api/forecast?latitude=${latitude}&longitude=${longitude}`
+  );
 
   return (
     <div>
-      <code>
-        <pre>{JSON.stringify(data, null, 2)}</pre>
-      </code>
+      {loading ? (
+        <Loading />
+      ) : (
+        <code>
+          <pre>{JSON.stringify(data, null, 2)}</pre>
+        </code>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default index
+export default index;
