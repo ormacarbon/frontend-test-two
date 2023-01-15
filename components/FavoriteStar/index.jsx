a2be4d2c19651e9b3f845d285b50adc6730d4928
costalucs <lucasdeslz@gmail.com>
2023-01-15 16:23:31 -0300

    fix: color favorite star

--- components/FavoriteStar/index.jsx
+++ components/FavoriteStar/index.jsx
@@ -1,11 +1,13 @@
-import { useContext, useState } from 'react'
+import { useContext } from 'react'
 import { PokemonContext } from '../../context/context'
 import { AiOutlineStar, AiFillStar } from 'react-icons/ai'
 import * as S from './styles'
+import { ThemeContext } from 'styled-components';
 
 
 export default function FavoriteStar({ pokemonName }) {
   const { handleFavorite, favoritesPokemons } = useContext(PokemonContext);
+  const { colors } = useContext(ThemeContext);
 
   const isFav = favoritesPokemons.some(favoritesPokemons => favoritesPokemons === pokemonName)
 
@@ -15,12 +17,14 @@ export default function FavoriteStar({ pokemonName }) {
     >
       {!isFav ?
         <AiOutlineStar
+          color={colors.textColor}
           size={22}
         />
         :
         <AiFillStar
+          color={colors.textColor}
+
           size={26}
-          color={'black'}
         />
       }
     </S.DivHeart>
