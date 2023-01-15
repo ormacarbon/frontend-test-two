b613932699a3896c2b5a5388b4126451fa483e8a
FOXXX <75998678+devrodrigomolina@users.noreply.github.com>
2023-01-15 20:39:59 -0300

    feat: Criado componente do menu mobile

---
+++ src/components/MenuMobile/MenuMobile.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import * as C from './style'
+
+const MenuMobile = () => {
+  return (
+    <C.ContainerMenuMobile>
+        <C.Burger>
+            <div class="strip burger-strip">
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+        </C.Burger>
+    </C.ContainerMenuMobile>
+  )
+}
+
+export default MenuMobile
