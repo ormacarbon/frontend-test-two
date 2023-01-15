6957e153b7dccf8d98b5c07da1c22b3fe0cf1781
FOXXX <75998678+devrodrigomolina@users.noreply.github.com>
2023-01-15 20:40:58 -0300

    refactor: Importado componente menu mobile

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,5 @@
 import Header from "../components/Header/Header"
+import MenuMobile from "../components/MenuMobile/MenuMobile"
 import ThemeProvider from "../contexts/themeContext"
 import { GlobalStyles } from "../styles/GlobalStyle"
 
@@ -8,6 +9,7 @@ function MyApp({ Component, pageProps }) {
     <>
     <ThemeProvider>
       <Header />
+      <MenuMobile />
       <Component {...pageProps} />
       <GlobalStyles />
     </ThemeProvider>
