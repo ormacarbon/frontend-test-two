6996d973af76cbbba8e70ca91a892988f4d861d0
FOXXX <75998678+devrodrigomolina@users.noreply.github.com>
2023-01-15 20:41:39 -0300

    refactor: Importado componente de graficos

--- src/pages/airpollution/index.js
+++ src/pages/airpollution/index.js
@@ -1,29 +1,54 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { useFetch } from "../../hooks/useFetch";
-import * as C from './style'
-import useLocation from '../../hooks/useLocation'
+import * as C from "./style";
+import useLocation from "../../hooks/useLocation";
+import Loading from '../../components/Loading/Loading'
+import { ThemeContext } from "../../contexts/themeContext";
+import Title from "../../components/Title/Title";
+import Charts from "../../components/Charts/Charts";
 
 const AirPolluition = () => {
+  const { theme } = useContext(ThemeContext)
   const { latitude, longitude } = useLocation();
-  const { data, error, loading } = useFetch(`/api/air_pollution?latitude=${latitude}&longitude=${longitude}`)
-
+  const { data, error, loading } = useFetch(
+    `/api/air_pollution?latitude=${latitude}&longitude=${longitude}`
+  );
 
   return (
-    <C.ContainerAirPolluition>
-      {data?.list?.map(elements => (
+    <C.ContainerAirPolluition style={{background: theme.background, color: theme.text}}>
+      {loading ? (
+        <Loading />
+      ) : (
         <>
-          <h1>CO: {elements.components.co}</h1>
-          <h1>NH3: {elements.components.nh3}</h1>
-          <h1>NO: {elements.components.no}</h1>
-          <h1>NO2: {elements.components.no2}</h1>
-          <h1>O3: {elements.components.o3}</h1>
-          <h1>pm2_5: {elements.components.pm2_5}</h1>
-          <h1>pm10: {elements.components.pm10}</h1>
-          <h1>so2: {elements.components.so2}</h1>
-        </>
-      ))}
-    </C.ContainerAirPolluition>
-  )
-}
+          <Title text="Air pollution data" />
+          {data?.list?.map((elements) => (
+            <div className="infosPolluition">
 
-export default AirPolluition
+
+            <Charts
+                elements={elements.components}
+                type="line"
+                width="500"
+              />
+            <Charts
+                elements={elements.components}
+                type="area"
+                width="500"
+              />
+              {/* <h1>CO: {elements.components.co}</h1>
+              <h1>NH3: {elements.components.nh3}</h1>
+              <h1>NO: {elements.components.no}</h1>
+              <h1>NO2: {elements.components.no2}</h1>
+              <h1>O3: {elements.components.o3}</h1>
+              <h1>pm2_5: {elements.components.pm2_5}</h1>
+              <h1>pm10: {elements.components.pm10}</h1>
+              <h1>so2: {elements.components.so2}</h1> */}
+            </div>
+          ))}
+        </>
+      )}
+    </C.ContainerAirPolluition>
+  );
+};
+
+export default AirPolluition;
