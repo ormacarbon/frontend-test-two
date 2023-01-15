42ae7330f1e1c1bd0aca138f5133bff52bdbd4b1
FOXXX <75998678+devrodrigomolina@users.noreply.github.com>
2023-01-15 20:39:08 -0300

    refactor: Importado tema do contexto e feito verificação para mostrar icone correto

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,36 +1,40 @@
-import Link from "next/link"
-import * as C from './style'
-import { BsFillMoonFill, BsFillSunFill } from 'react-icons/bs'
-import { ThemeContext } from "../../contexts/themeContext"
-import { useContext } from "react"
+import * as C from "./style";
+import Link from "next/link";
+import { BsFillMoonFill, BsFillSunFill } from "react-icons/bs";
+import { ThemeContext } from "../../contexts/themeContext";
+import { useContext } from "react";
 
 const linksMenu = [
-  {name: 'Home', page: '/'},
-  {name: 'Collaborators', page: '/collaborators'},
-  {name: 'Air Pollution', page: '/airpollution'},
-  {name: 'Forecast', page: '/forecast'},
-]
+  { name: "Home", page: "/" },
+  { name: "Collaborators", page: "/collaborators" },
+  { name: "Air Pollution", page: "/airpollution" },
+  { name: "Forecast", page: "/forecast" },
+];
 
 const Header = () => {
-  const { theme, setTheme } = useContext(ThemeContext)
-
+  const { theme, setTheme } = useContext(ThemeContext);
 
   return (
     <C.ContainerHeader>
       <C.HeaderNavigation>
-        <Link href='/' className="logo">OrmaCarbon</Link>
+        <Link href="/" className="logo">
+          OrmaCarbon
+        </Link>
         <C.ListItems>
-          {linksMenu.map(linkItems => (
+          {linksMenu.map((linkItems) => (
             <Link href={linkItems.page}>{linkItems.name}</Link>
           ))}
         </C.ListItems>
       </C.HeaderNavigation>
       <C.ChangeTheme>
-        <BsFillMoonFill onClick={() => setTheme('dark')} />
-        <BsFillSunFill onClick={() => setTheme('light')}/>
+        {theme.background === "black" ? (
+          <BsFillSunFill onClick={() => setTheme("light")} />
+        ) : (
+          <BsFillMoonFill onClick={() => setTheme("dark")} />
+        )}
       </C.ChangeTheme>
     </C.ContainerHeader>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
