74b53a738802bfe4c292f7e6b5dd6524abb1b02b
Ivan Gomes <ivanjunior.pt@gmail.com>
2023-01-15 08:33:14 -0300

    Removing unnecessary code from global styles

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,45 +1,23 @@
 import { createGlobalStyle } from 'styled-components'
 
-const sizes = {
-  mobileS: '320px',
-  mobileM: '375px',
-  mobileL: '425px',
-  tablet: '768px',
-  laptop: '1024px',
-  laptopL: '1440px',
-  desktop: '2560px'
-}
-
-export const devices = {
-  mobileS: `(min-width: ${sizes.mobileS})`,
-  mobileM: `(min-width: ${sizes.mobileM})`,
-  mobileL: `(min-width: ${sizes.mobileL})`,
-  tablet: `(min-width: ${sizes.tablet})`,
-  laptop: `(min-width: ${sizes.laptop})`,
-  laptopL: `(min-width: ${sizes.laptopL})`,
-  desktop: `(min-width: ${sizes.desktop})`
-}
-
 export const GlobalStyle = createGlobalStyle`
-    * {
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-    }
+     html {
+    box-sizing: border-box;
+    font-size: 62.5%;
+  }
 
-    html { // Adapt font size for different screen sizes
-        @media (max-width: 1080px) {
-            font-size: 93.75% //15px
-        }
-        @media (max-width: 720px) {
-            font-size: 87.5% // 14px
-        }
-    }
+  *,
+  *::before,
+  *::after {
+    box-sizing: inherit;
+    margin: 0;
+    padding: 0;
+  }
 
     body {
-      background: ${(props) => props.theme['white']};
+      background: ${(props) => props.theme.body};
       -webkit-font-smoothing: antialiased;
-      color: ${(props) => props.theme['white']}
+      color: ${(props) => props.theme.text}
     }
 
     body, input, textarea, button {
