f057ca66676ddb045bfbe4fde15ff614d0f2d5e2
Elison Michell <dev.elisonmichell@gmail.com>
2023-01-15 17:11:09 -0300

    feat(document): add icon and head elements

--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,11 @@ export default function Document() {
   return (
     <Html>
       <Head>
+        <meta name="application-name" content="Pokémon Universe" />
+        <meta name="description" content="Your definitive guide to the universe of Pokémons" />
+        <link rel="author" href="https://github.com/ElisonMichell" />
+        <link rel="icon" sizes="192x192" href="/pokeball-logo.svg"></link>
+
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
