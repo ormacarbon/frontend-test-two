c1382ebdafe9e3790203b01c739c84840fff3d38
Ivan Gomes <ivanjunior.pt@gmail.com>
2023-01-15 08:22:18 -0300

    Adding skeleton loading

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,42 +1,48 @@
 import { useMovies } from 'contexts/MoviesContext'
 
-import * as S from './home.styles'
+import * as S from 'styles/pageStyles/home.styles'
+
+import Link from 'next/link'
 
 import { Filters } from 'components/Filters'
-import { Header } from 'components/Header'
 import { MovieCard } from 'components/MovieCard'
 import { Pagination } from 'components/Pagination'
+import { LoadingSkeleton } from 'components/LoadingSkelenton'
 
 import { tmdbImage } from 'constants/url'
-import Link from 'next/link'
 
 export default function Home() {
   const { movies, isLoading } = useMovies()
 
   return (
     <S.Container>
-      <Header />
       <Filters />
 
-      <S.CardContainer>
-        {isLoading ? (
-          <p>Loading...</p>
-        ) : (
-          movies?.results?.map((movie) => (
-            <Link key={movie.id} href={`/movie/${movie.id}`}>
-              <MovieCard
-                title={movie.title}
-                date={movie.release_date}
-                poster={tmdbImage(movie.poster_path, 185)}
-              />
-            </Link>
-          ))
-        )}
-      </S.CardContainer>
+      {isLoading ? (
+        <S.LoadingContainer>
+          <LoadingSkeleton />
+        </S.LoadingContainer>
+      ) : (
+        <S.CardContainer>
+          <S.CardWrap>
+            {movies?.results.map((movie) => (
+              <Link key={movie.id} href={`/movie/${movie.id}`}>
+                <MovieCard
+                  title={movie.title}
+                  date={movie.release_date}
+                  poster={tmdbImage(movie.poster_path, 185)}
+                />
+              </Link>
+            ))}
+          </S.CardWrap>
+        </S.CardContainer>
+      )}
 
-      <S.PaginationContainer>
-        <Pagination />
-      </S.PaginationContainer>
+      {isLoading === false && (
+        <S.PaginationContainer>
+          <Pagination />
+        </S.PaginationContainer>
+      )}
     </S.Container>
   )
 }
