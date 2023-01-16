8ad2350b9f1179826866353759a81a19e2d5db35
Geraldo Pereira da Costa Neto <80066270+GeracNeto@users.noreply.github.com>
2023-01-16 20:51:59 -0300

    front: updated 'AnotherEntertainment'
    -passed the values ​​obtained in the api call to display on the screen
    -call the map function to display the cards of movies and series
    -created  condition to verify if the data are from a movie or series

--- src/components/AnotherEntertainment/index.tsx
+++ src/components/AnotherEntertainment/index.tsx
@@ -1,12 +1,49 @@
-export default function AnotherEntertainment() {
+import { DataProps } from "../../pages/movies";
+
+import Card from "../Card";
+
+import { MainAnotherEntertainment, Cards } from "./styles";
+
+interface EntertainmentProps {
+  data: DataProps[];
+  movieOrSerie: string;
+}
+
+export default function AnotherEntertainment({
+  data,
+  movieOrSerie,
+}: EntertainmentProps) {
+  const dataSliced = data.slice(0, 12);
+
   return (
-    <main>
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestiae et,
-        accusamus libero sint quasi ex enim, quod quisquam ratione pariatur,
-        corporis quidem nam ipsam. Ratione earum sapiente ipsam harum
-        distinctio!
-      </p>
-    </main>
+    <MainAnotherEntertainment>
+      <Cards>
+        {dataSliced.length > 0 ? (
+          dataSliced.map((item) => {
+            // This condition is for identify if the API is 'serie' or movie'. Because the key changes
+            let titleOrName = "";
+
+            if (movieOrSerie === "serie") {
+              const { name } = item;
+              titleOrName = name;
+            } else if (movieOrSerie === "movie") {
+              const { title } = item;
+              titleOrName = title;
+            }
+
+            return (
+              <Card
+                key={item.id}
+                poster={item.poster_path}
+                title={titleOrName}
+                vote={item.vote_average}
+              />
+            );
+          })
+        ) : (
+          <p>carregando</p>
+        )}
+      </Cards>
+    </MainAnotherEntertainment>
   );
 }
