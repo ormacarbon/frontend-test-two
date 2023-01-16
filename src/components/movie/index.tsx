44798e754d6ed5160b4445ecb0bbd7870ec940ab
thiago nunes <desenvolvedor.nunes@gmail.com>
2023-01-16 15:38:12 -0300

    feat: add button to bookmark movie and link to get more movie info

--- src/components/movie/index.tsx
+++ src/components/movie/index.tsx
@@ -1,29 +1,43 @@
+import { Button } from '@components/buttons/addItem';
+import Link from 'next/link';
 import * as Styled from './styled';
 
 export type MovieProps = {
   src: string
   title: string
+  movieId: string
   release_date: string
   vote_average: number
+  actionOnClick: () => void
 };
 
 export function Movie({
   src,
   title,
+  movieId,
   release_date,
   vote_average,
+  actionOnClick
 }: MovieProps): JSX.Element {
 
   return (
     <Styled.Container>
       <Styled.MovieInfo>
-        <img src={src} alt="movie image" />
-        <Styled.TextSection>
-          <p>{title}</p>
-          <p>Lançado em: {release_date}</p>
-        </Styled.TextSection>
+        <Link href={{
+          pathname: `/bm-play/movie/${movieId}`,
+        }} >
+          <img src={src} alt="movie image" />
+          <Styled.TextSection>
+            <p>{title}</p>
+            <p>Lançado em: {release_date}</p>
+          </Styled.TextSection>
+        </Link>
       </Styled.MovieInfo>
       <p>Nota do Público: {vote_average}</p>
+      <Button
+        textButton='Adicionar aos Favoritos'
+        actionOnClick={actionOnClick} 
+      />
     </Styled.Container>
   );
 }
