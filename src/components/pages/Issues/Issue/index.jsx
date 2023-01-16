5b8fb1a8099fb65f00516fb11cc3ad8a0308d36f
Diêgo Raian <diegofrr.19@gmail.com>
2023-01-16 00:56:48 -0300

    [feat] Adição de ícone para identificar status da issue

--- src/components/pages/Issues/Issue/index.jsx
+++ src/components/pages/Issues/Issue/index.jsx
@@ -1,22 +1,33 @@
+import { useRouter } from "next/router";
 import { Container, Content, Label } from "./styles";
+import {
+  IssueOpenedIcon as Opened,
+  IssueClosedIcon as Closed,
+} from "@primer/octicons-react";
 
 export default function Issue({ data }) {
-  console.log(data.labels);
+  const router = useRouter();
+  const state = data.state;
+
   return (
-    <Container style={{ fontSize: 30 }}>
-      <Content>
+    <Container title="Abrir issue" href={data.html_url} target="_blank">
+      <Content state={data.state}>
         <img className="profile_img" src={data.user.avatar_url} />
         <div>
           <span className="user_name">{data.user.login}</span>
           <p className="issue_title">
             {data.title} <br />
-            {data.labels.map((label) => (
-              <span key={`${label.id}`}>
-                <Label label={label}>{label.name}</Label>
+            {data.labels.map((label, i) => (
+              <span key={i}>
+                <Label>{label.name}</Label>
               </span>
             ))}
           </p>
+          <span className="issue_id">#{data.id}</span>
         </div>
+        <span className="issue_state">
+          {state === "open" ? <Opened /> : <Closed />}
+        </span>
       </Content>
     </Container>
   );
--- src/components/pages/Issues/Issue/styles.js
+++ src/components/pages/Issues/Issue/styles.js
@@ -1,18 +1,31 @@
 import styled from "styled-components";
 
-export const Container = styled.div`
-  max-width: 800px;
-  width: 100%;
+export const Container = styled.a`
   border: 1px solid ${(p) => p.theme.colors.header.border};
   border-radius: 10px;
-  padding: 10px;
+  cursor: pointer;
+  transition: all 0.3s ease;
+  outline: none;
+  width: 100%;
+
+  :hover,
+  :focus {
+    border-color: ${(p) => p.theme.colors.blue[200]};
+    .user_name,
+    .issue_title,
+    .issue_id {
+      color: ${(p) => p.theme.colors.blue[200]};
+    }
+  }
 `;
 
 export const Content = styled.div`
+  margin: 10px;
   display: flex;
   justify-content: left;
   flex-direction: row;
   gap: 10px;
+  position: relative;
 
   .profile_img {
     width: 40px;
@@ -30,12 +43,41 @@ export const Content = styled.div`
     .user_name {
       font-size: 12px;
       color: ${(p) => p.theme.colors.issue.user};
+      transition: all 0.3s ease;
     }
 
     .issue_title {
+      transition: all 0.3s ease;
       font-size: 16px;
       font-weight: 600;
       color: ${(p) => p.theme.colors.issue.title};
+    }
+
+    .issue_id {
+      color: ${(p) => p.theme.colors.secundary};
+      font-size: 0.6rem;
+      position: absolute;
+      right: 0;
+      top: 0;
+    }
+  }
+
+  .issue_state {
+    background-color: ${(p) => p.theme.colors.bg};
+    position: absolute;
+    left: -20px;
+    top: 10px;
+    width: 20px;
+    height: 20px;
+    display: grid;
+    place-items: center;
+    border-radius: 50%;
+
+    svg {
+      width: 12px;
+      height: 12px;
+      color: ${(issueProps) =>
+        issueProps.state === "open" ? "#3fb950" : "red"};
     }
   }
 `;
