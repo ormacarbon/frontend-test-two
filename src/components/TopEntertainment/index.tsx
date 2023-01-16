1f6fff5653264f888a540253e25a1f3ba659dd7b
Geraldo Pereira da Costa Neto <80066270+GeracNeto@users.noreply.github.com>
2023-01-16 20:55:01 -0300

    front: update 'TopEntertainment'
    -created condition to verify if the data are from a movie or series
    -call made to the API to get the trailer of the movie or series
    -displayed on screen via the react-youtube lib the most popular movie or serie

--- src/components/TopEntertainment/index.tsx
+++ src/components/TopEntertainment/index.tsx
@@ -1,18 +1,71 @@
-import { AsideTop } from "./styles";
+import { AsideTop, Info } from "./styles";
 
-export default function TopEntertainment() {
+import Youtube from "react-youtube";
+
+import { useEffect, useState } from "react";
+
+import { AiFillStar } from "react-icons/ai";
+
+import axios from "axios";
+
+import { DataProps } from "../../pages/movies";
+
+interface TopProps {
+  data: DataProps[];
+  movieOrSerie: string;
+}
+
+export default function TopEntertainment({ data, movieOrSerie }: TopProps) {
+  const topTrailer = data[0]; // Get the most popular movie
+  const [trailer, setTrailer] = useState();
+  let titleOrName = "";
+
+  if (movieOrSerie === "movie") {
+    const { title } = topTrailer;
+    titleOrName = title;
+  } else if (movieOrSerie === "tv") {
+    const { name } = topTrailer;
+    titleOrName = name;
+  }
+
+  useEffect(() => {
+    async function getTrailer() {
+      try {
+        const response = await axios.get(
+          `https://api.themoviedb.org/3/${movieOrSerie}/${topTrailer.id}/videos?api_key=b59de4554604a026d4521e1afaf9d6b3`
+        );
+        setTrailer(response.data.results[0].key);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    getTrailer();
+  }, [data, movieOrSerie, topTrailer.id]);
+
+  console.log(movieOrSerie);
+
   return (
     <AsideTop>
-      <img
-        src="https://m.media-amazon.com/images/M/MV5BNzk1OGU2NmMtNTdhZC00NjdlLWE5YTMtZTQ0MGExZTQzOGQyXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_.jpg"
-        alt="top"
+      <Youtube
+        videoId={trailer}
+        opts={{
+          playerVars: {
+            autoplay: 1,
+            loop: 1,
+            controls: 0,
+          },
+        }}
       />
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestiae et,
-        accusamus libero sint quasi ex enim, quod quisquam ratione pariatur,
-        corporis quidem nam ipsam. Ratione earum sapiente ipsam harum
-        distinctio!
-      </p>
+      <Info>
+        <h2>{titleOrName}</h2>
+        <span>
+          {topTrailer.vote_average}
+          <AiFillStar size={16} color="#fde047" />
+        </span>
+
+        <p>{topTrailer.overview}</p>
+      </Info>
     </AsideTop>
   );
 }
