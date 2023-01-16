cfb22f7bf08088299e3ad02e53495003cf4d1f3d
= <f7lipe@me.com>
2023-01-15 21:57:57 -0300

    fix: Category
    Includes href

--- components/Category/index.jsx
+++ components/Category/index.jsx
@@ -1,10 +1,10 @@
 import { CategoryBody, Title } from "./styles"
 import { HStack } from "../../shared/styles/sharedStyles"
 
-const Category = ({ title, icon }) => {
+const Category = ({ title, icon, href }) => {
     return (
         <CategoryBody
-            href={`/estimate/${title}`}>
+            href={`/estimate/${href}`}>
             <HStack
                 justify="space-evenly" >
                 {icon}
