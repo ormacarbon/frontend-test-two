61a046779bb5852851bdbeee3be906c3793ddd82
Geraldo Pereira da Costa Neto <80066270+GeracNeto@users.noreply.github.com>
2023-01-16 20:35:36 -0300

    styles: updated global
    -added styles for body, input and button
    -created styles for 'MainContent' with grid layout
    -created styles for all 'Links' from nextjs

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,4 +1,6 @@
-import styled, { createGlobalStyle } from "styled-components";
+import styled, { createGlobalStyle } from 'styled-components';
+
+import Link from 'next/link';
 
 export default createGlobalStyle`
 @import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap');
@@ -19,11 +21,36 @@ export default createGlobalStyle`
     a{
       text-decoration: none;
     }
+
+    body, input, button{
+      font-family: 'Inter', sans-serif;
+      font-weight: 400;
+      font-size: 1rem;
+  }
 `;
 
 // This content will repeat for movies, series and tv shows pages
 export const MainContent = styled.main`
   margin-top: 2rem;
-  display: flex;
-  gap: 2rem;
+  padding: 2rem;
+  height: 80vh;
+
+  display: grid; 
+  grid-template-columns: 1fr 1fr 1fr; 
+  grid-template-rows: 1fr 1fr 1fr; 
+  
+  grid-template-areas: 
+    "main main aside"
+    "main main aside"
+    "main main aside"; 
+`;
+
+// Global style to Link
+export const StyledLink = styled(Link)`
+  color: ${(props) => props.theme.colors.white};
+
+  &:hover{
+    color: ${(props) => props.theme.colors['gray-300']};
+    transition: 0.2s;
+  }
 `;
