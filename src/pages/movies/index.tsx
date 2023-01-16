e9b1d35def0293c4c108888f2448892bb0c38126
Geraldo Pereira da Costa Neto <80066270+GeracNeto@users.noreply.github.com>
2023-01-16 20:42:53 -0300

    front: Updated pages 'movie' and 'series'
    -Added API to call the list of movies and series

--- src/pages/movies/index.tsx
+++ src/pages/movies/index.tsx
@@ -1,15 +1,51 @@
 import AnotherEntertainment from "../../components/AnotherEntertainment";
 import Header from "../../components/Header";
 import TopEntertainment from "../../components/TopEntertainment";
+
 import { MainContent } from "../../styles/global";
 
+import { useEffect, useState } from "react";
+
+import axios from "axios";
+
+export interface DataProps {
+  id: number;
+  title: string;
+  name: string;
+  poster_path: string;
+  vote_average: number;
+  overview: string;
+}
+
 export default function Movies() {
+  const [movies, setMovies] = useState<DataProps[]>();
+
+  useEffect(() => {
+    async function getPopularMovies() {
+      try {
+        const response = await axios.get(
+          "https://api.themoviedb.org/3/movie/popular?api_key=b59de4554604a026d4521e1afaf9d6b3"
+        );
+        setMovies(response.data.results);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    getPopularMovies();
+  }, []);
+
   return (
     <>
-      <Header />
+      <Header showSearch={true} />
+
       <MainContent>
-        <AnotherEntertainment />
-        <TopEntertainment />
+        {movies && (
+          <>
+            <AnotherEntertainment data={movies} movieOrSerie={"movie"} />
+            <TopEntertainment data={movies} movieOrSerie={"movie"} />
+          </>
+        )}
       </MainContent>
     </>
   );
--- src/pages/series/index.tsx
+++ src/pages/series/index.tsx
@@ -1,10 +1,44 @@
+import AnotherEntertainment from "../../components/AnotherEntertainment";
 import Header from "../../components/Header";
+import TopEntertainment from "../../components/TopEntertainment";
+
+import { MainContent } from "../../styles/global";
+
+import { useEffect, useState } from "react";
+
+import axios from "axios";
+
+import { DataProps } from "../movies";
 
 export default function Series() {
+  const [series, setSeries] = useState<DataProps[]>();
+
+  useEffect(() => {
+    async function getPopularSeries() {
+      try {
+        const response = await axios.get(
+          "https://api.themoviedb.org/3/tv/popular?api_key=b59de4554604a026d4521e1afaf9d6b3"
+        );
+        setSeries(response.data.results);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    getPopularSeries();
+  }, []);
+
   return (
     <>
-      <Header />
-      <h1>series</h1>
+      <Header showSearch={true} />
+      <MainContent>
+        {series && (
+          <>
+            <AnotherEntertainment data={series} movieOrSerie={"serie"} />
+            <TopEntertainment data={series} movieOrSerie={"tv"} />
+          </>
+        )}
+      </MainContent>
     </>
   );
 }
