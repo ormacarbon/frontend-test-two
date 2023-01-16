9ec262dfc3a4c4fead05ca8342754a541332d22e
Diêgo Raian <diegofrr.19@gmail.com>
2023-01-16 02:32:12 -0300

    [feat] Adição de Dropdrown

---
+++ src/components/pages/Issues/Dropdown/index.jsx
@@ -0,0 +1,54 @@
+import { useEffect, useRef, useState } from "react";
+import { Container, Options, Option, Select, Background } from "./styles";
+import { TriangleDownIcon as DownIcon } from "@primer/octicons-react";
+
+export default function IssueTypeDropdown(props) {
+  const select = useRef();
+  const [margin, setMargin] = useState();
+  const [openDropdrown, setOpenDropdown] = useState(false);
+  const state = (state) => {
+    return state === "open"
+      ? "Abertas"
+      : state === "closed"
+      ? "Fechadas"
+      : "Todas";
+  };
+  const options = ["all", "open", "closed"];
+
+  useEffect(() => {
+    setMargin(select.current?.clientWidth);
+  }, [state]);
+
+  function handleChangeState(state) {
+    props.setState(state);
+    toggleDropdown();
+  }
+
+  function toggleDropdown() {
+    setOpenDropdown(!openDropdrown);
+  }
+
+  return (
+    <>
+      <Container>
+        <Select ref={select} onClick={toggleDropdown}>
+          {state(props.state)} <DownIcon />
+        </Select>
+        {openDropdrown && (
+          <Options margin={margin}>
+            {options.map((op, i) => (
+              <Option
+                selected={op === props.state}
+                onClick={() => handleChangeState(op)}
+                key={i}
+              >
+                {state(op)}
+              </Option>
+            ))}
+          </Options>
+        )}
+      </Container>
+      {openDropdrown && <Background onClick={toggleDropdown} />}
+    </>
+  );
+}
--- src/pages/issues/[repositoryName].jsx
+++ src/pages/issues/[repositoryName].jsx
@@ -1,18 +1,27 @@
+import axios from "axios";
+import Head from "next/head";
+import Issue from "../../components/pages/Issues/Issue";
 import repo_data from "../../utils/repositoryData";
 import issue_data from "../../utils/issuesData";
-import Issue from "../../components/pages/Issues/Issue";
-
-import axios from "axios";
-
-import { useRouter } from "next/router";
-import { Container, Content, IssuesContainer } from "./styles";
-import { useEffect, useState } from "react";
-import issuesData from "../../utils/issuesData";
-import Head from "next/head";
+import IssueTypeDropdown from "../../components/pages/Issues/Dropdown";
 import RepositoryIssuesHeader from "../../components/shared/RepositoryIssuesHeader";
+
+import { FiArrowLeft, FiArrowRight } from "react-icons/fi";
+import { ColorRing } from "react-loader-spinner";
+import { useRouter } from "next/router";
+import {
+  ActionsContainer,
+  Container,
+  Content,
+  IssuesContainer,
+  PaginationButtons,
+} from "./styles";
+import { useContext, useEffect, useRef, useState } from "react";
+import { ThemeContext } from "../../contexts/theme";
 
 export default function repositoryNameIssues() {
   const { repositoryName } = useRouter().query;
+  const { theme } = useContext(ThemeContext);
 
   const [repository, setRepository] = useState({});
   const [issues, setIssues] = useState([]);
@@ -23,12 +32,14 @@ export default function repositoryNameIssues() {
   useEffect(() => {
     if (repositoryName) {
       (async () => {
+        setLoading(true);
         // const [repositoryData, issuesData] = await Promise.all([
         //   axios.get(`https://api.github.com/repos/${repositoryName}`),
         //   axios.get(`https://api.github.com/repos/${repositoryName}/issues`, {
         //     params: {
-        //       state: "all",
+        //       state,
         //       per_page: 15,
+        //       page,
         //     },
         //   }),
         // ]);
@@ -38,9 +49,15 @@ export default function repositoryNameIssues() {
 
         setIssues(issue_data);
         setRepository(repo_data);
+        setLoading(false);
       })();
     }
-  }, [repositoryName]);
+  }, [repositoryName, page, state]);
+
+  function handleChangeIssuesPage(_page) {
+    setPage(page + _page);
+    console.log(page);
+  }
 
   return (
     <>
@@ -52,10 +69,33 @@ export default function repositoryNameIssues() {
       {repository?.name && <RepositoryIssuesHeader repository={repository} />}
       <Container>
         <Content>
+          <ActionsContainer>
+            <IssueTypeDropdown state={state} setState={setState} />
+            <PaginationButtons>
+              {page > 1 && (
+                <button onClick={() => handleChangeIssuesPage(-1)}>
+                  <FiArrowLeft /> Anterior
+                </button>
+              )}
+              <button onClick={() => handleChangeIssuesPage(1)}>
+                Próxima
+                <FiArrowRight />
+              </button>
+            </PaginationButtons>
+          </ActionsContainer>
           <IssuesContainer>
-            {issues.map((issue) => (
-              <Issue key={issue.id} data={issue} />
-            ))}
+            {loading ? (
+              <ColorRing
+                visible={true}
+                height="60"
+                width="60"
+                ariaLabel="blocks-loading"
+                wrapperClass="blocks-wrapper"
+                colors={theme.colors.blue[200].repeat(5).match(/.{1,7}/g)}
+              />
+            ) : (
+              issues.map((issue) => <Issue key={issue.id} data={issue} />)
+            )}
           </IssuesContainer>
         </Content>
       </Container>
