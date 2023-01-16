dc5de05d3872659090a31bfdb7eca2bcf2f7f1a0
Geraldo Pereira da Costa Neto <80066270+GeracNeto@users.noreply.github.com>
2023-01-16 20:33:01 -0300

    front: created page 'Welcome'

---
+++ src/components/Welcome/index.tsx
@@ -0,0 +1,23 @@
+import { WelcomeMain } from "./styles";
+
+import { StyledLink } from "../../styles/global";
+
+import { AiFillStar } from "react-icons/ai";
+
+export default function Welcome() {
+  return (
+    <WelcomeMain>
+      <h1>
+        Welcome to
+        <span> Cine</span>
+        phile
+      </h1>
+      <footer>
+        <StyledLink href="/movies">
+          Lets get start!
+          <AiFillStar size={16} color="#fde047" />
+        </StyledLink>
+      </footer>
+    </WelcomeMain>
+  );
+}
