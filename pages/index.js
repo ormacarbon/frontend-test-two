3cd710e0d0008b068817e18768f3bfeb4b3ce9b2
Caroline Maia Abreu da Silva <carolmas99@gmail.com>
2023-01-16 14:16:23 -0300

    styles: home styles added

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import { useContext } from "react";
 import { NameContext } from "../components/Context";
+import { HomeContainer } from "../styles/style";
 
 export default function Home() {
   const { name, setName, user, setUser } = useContext(NameContext);
@@ -9,17 +10,21 @@ export default function Home() {
   }
 
   return (
-    <div>
+    <HomeContainer>
       {user ? (
         <h1>Welcome to my pokemon page, {user}</h1>
       ) : (
         <h1>Welcome to my pokemon page</h1>
       )}
-      <div>
-        <h4>Type yout name:</h4>
-        <input type="text" onChange={(event) => setName(event.target.value)} />
+      <h3>Type your name:</h3>
+      <div className="form">
+        <input
+          type="text"
+          placeholder="Name"
+          onChange={(event) => setName(event.target.value)}
+        />
         <button onClick={handleShowName}>Click</button>
       </div>
-    </div>
+    </HomeContainer>
   );
 }
---
+++ styles/style.js
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+
+export const HomeContainer = styled.div`
+  h1 {
+    margin-bottom: 3rem;
+    margin-top: 3rem;
+  }
+
+  h3 {
+    margin-bottom: 1rem;
+  }
+
+  .form {
+    display: flex;
+    justify-content: center;
+    gap: 0.5rem;
+
+    input {
+      padding: 0.7rem;
+      width: 60%;
+    }
+
+    button {
+      width: 25%;
+    }
+  }
+`;
