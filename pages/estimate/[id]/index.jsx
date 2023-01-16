8ffdcaed74020c4375934a1147e1c92695bad8b0
= <f7lipe@me.com>
2023-01-16 18:36:06 -0300

    Update Estimate page
    Includes a section title

--- pages/estimate/[id]/index.jsx
+++ pages/estimate/[id]/index.jsx
@@ -1,11 +1,12 @@
 import { Form, Button } from "../../../shared/inputs/styles"
-import { Main } from "../../../shared/styles/sharedStyles"
+import { Main, SectionTitle } from "../../../shared/styles/sharedStyles"
 import { useForm } from "react-hook-form"
 import { useRouter } from "next/router"
 import { HStack } from "../../../shared/styles/sharedStyles"
 import { estimate } from "../../../shared/services/api"
 import { useState } from "react"
 import { Rings } from "react-loader-spinner"
+import { Itemlist, Item } from "../../../components/InfoCard/styles"
 import EnergyInput from "../../../shared/inputs/EnergyInput"
 import useLocation from "../../../hooks/useLocation"
 import InfoCard from "../../../components/InfoCard"
@@ -19,12 +20,12 @@ const EstimatePage = () => {
     const [estimatedData, setEstimatedData] = useState(null)
     const [loading, setLoading] = useState(false)
     const [error, setError] = useState(null)
-    
+
     const renderInputs = {
         "energy": <EnergyInput register={register} />,
     }
 
-    const queryBuilder = {
+    const categoryParams = {
         "energy": {
             emission_factor: {
                 id: "electricity-energy_source_grid_mix",
@@ -35,37 +36,63 @@ const EstimatePage = () => {
             }
         }
     }
-    
+
     const onSubmit = async (content) => {
         const data = {
-            emission_factor: queryBuilder[id].emission_factor,
-            parameters: {...content, ...queryBuilder[id].parameters}
+            emission_factor: categoryParams[id].emission_factor,
+            parameters: { ...content, ...categoryParams[id].parameters }
         }
 
-        try{
+        try {
             setLoading(true)
             const response = await estimate(data)
             setEstimatedData(response)
             setError(null)
-       } 
-         catch (error) {
-            setError(error)
-         }
-        finally {   
-                setLoading(false)
         }
+        catch (error) {
+            setError(error)
+        }
+        finally {
+            setLoading(false)
+        }
+    }
+
+    const generateInfoCard = () => {
+        const emissionFactor = estimatedData["emission_factor"]
+        const constituentGases = estimatedData["constituent_gases"]
+        delete constituentGases["co2e_total"]
+        return (
+
+            <Itemlist>
+                <li>Category: <span>{emissionFactor["category"]}</span></li>
+                <li>Region: <span>{emissionFactor["region"]}, {emissionFactor["year"]}</span></li>
+                <li>Source: <span>{emissionFactor["source"]}</span></li>
+
+                <h3>Constintuent gases</h3>
+
+                {
+                    Object.keys(constituentGases).map((gas, index) => {
+                        return (
+                            constituentGases[gas] && <Item key={index}> <h3> {gas}</h3> <p>{constituentGases[gas]} {estimatedData["co2e_unit"]}</p></Item>
+                        )
+                    })
+                }
+            </Itemlist>
+
+        )
     }
 
     return (
         <Main>
+            <SectionTitle>Carbon estimation of {id} </SectionTitle>
             <HStack wrap="wrap" justify="center">
                 <Form onSubmit={handleSubmit(onSubmit)}>
                     {renderInputs[id]}
-                    <Button type='submit'>{loading ? "Loading" : "Estimate"}</Button>
+                    <Button type='submit' disabled={loading}> Estimate </Button>
                 </Form>
                 {loading && <Rings color="#000" height={100} width={100} />}
-                {!error && estimatedData && <InfoCard data={estimatedData} />}
-                {error &&  <strong>No data available for the chosen region</strong>}
+                {!error && estimatedData &&  InfoCard(generateInfoCard())}
+                {error && <strong>No data available for the chosen region</strong>}
             </HStack>
         </Main>
     );
