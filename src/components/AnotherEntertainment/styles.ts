d9a237907fec04bc9cf646c7d8415a0453e4ab62
Geraldo Pereira da Costa Neto <80066270+GeracNeto@users.noreply.github.com>
2023-01-16 20:32:31 -0300

    styles: created styles of component 'AnotherEntertainment'

---
+++ src/components/AnotherEntertainment/styles.ts
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const MainAnotherEntertainment = styled.main`
+  grid-area: main;
+`;
+
+export const Cards = styled.div`
+  width: 100%;
+  height: 100%;
+
+  display: flex;
+  gap: 0.6rem;
+  align-items: center;
+  justify-content: flex-start;
+  align-content: space-between;
+  flex-wrap: wrap;
+`;
