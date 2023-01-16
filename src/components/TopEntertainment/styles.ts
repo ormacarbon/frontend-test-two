b4d70c7193b46c429417a352b60d8de776faafec
Geraldo Pereira da Costa Neto <80066270+GeracNeto@users.noreply.github.com>
2023-01-16 20:48:34 -0300

    styles: updated 'TopEntertainment'
    -created styles for asideTop to show video and info with grid
    -created styles for info div

--- src/components/TopEntertainment/styles.ts
+++ src/components/TopEntertainment/styles.ts
@@ -3,9 +3,27 @@ import styled from "styled-components";
 export const AsideTop = styled.aside`
   width: 100%;
 
-  img {
-    max-width: 100%;
-    height: auto;
-    object-fit: contain;
+  box-shadow: 0px 0px 6px 4px rgba(58, 24, 193, 0.61);
+  border-radius: 20px;
+
+  iframe {
+    width: 100%;
+    border-radius: 20px;
+  }
+
+  grid-area: aside;
+`;
+
+export const Info = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+
+  padding: 2rem;
+
+  span {
+    color: ${(props) => props.theme.colors["gray-400"]};
+    display: flex;
+    align-items: center;
   }
 `;
