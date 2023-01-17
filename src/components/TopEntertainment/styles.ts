9ae7c909dbbbf55b1070eedb0d758c5d96fc0362
Geraldo Pereira da Costa Neto <80066270+GeracNeto@users.noreply.github.com>
2023-01-17 12:07:32 -0300

    styles: updated to use img instead of video

--- src/components/TopEntertainment/styles.ts
+++ src/components/TopEntertainment/styles.ts
@@ -2,16 +2,26 @@ import styled from "styled-components";
 
 export const AsideTop = styled.aside`
   width: 100%;
+  padding: 1rem;
 
   box-shadow: 0px 0px 6px 4px rgba(58, 24, 193, 0.61);
   border-radius: 20px;
 
-  iframe {
-    width: 100%;
-    border-radius: 20px;
-  }
-
   grid-area: aside;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  align-items: center;
+
+  header {
+    display: flex;
+    align-items: center;
+    gap: 0.2rem;
+
+    h1 {
+      font-size: 2rem;
+    }
+  }
 `;
 
 export const Info = styled.div`
@@ -25,5 +35,21 @@ export const Info = styled.div`
     color: ${(props) => props.theme.colors["gray-400"]};
     display: flex;
     align-items: center;
+  }
+`;
+
+export const ImgDiv = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: start;
+  border-radius: 10px;
+  cursor: pointer;
+
+  img {
+    max-width: 30%;
+    height: auto;
+    object-fit: contain;
+    border-radius: 10px;
   }
 `;
