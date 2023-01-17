791c7c3638e0ce6cf34948c140cde20711751d5e
gabrielprmartins <gabrielpr.martins@gmail.com>
2023-01-17 08:00:08 -0300

    feat: added favicon

--- pages/_document.js
+++ pages/_document.js
@@ -10,7 +10,7 @@ const Document = () => {
           href="https://fonts.googleapis.com/css2?family=Mulish:wght@400;700&display=swap"
           rel="stylesheet"
         />
-        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="shortcut icon" href="/favicon.png" />
         <meta name="theme-color" content="#3E64FF" />
       </Head>
       <body>
