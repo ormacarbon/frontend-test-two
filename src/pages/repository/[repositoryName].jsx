e1173c17df4fb15a4ed92ddc8d135fe74ec0a6aa
Diêgo Raian <diegofrr.19@gmail.com>
2023-01-17 02:59:33 -0300

    [feat] Finalização da página de Repositório

--- src/pages/repository/[repositoryName].jsx
+++ src/pages/repository/[repositoryName].jsx
@@ -3,9 +3,10 @@ import Head from "next/head";
 import repo_data from "../../utils/repositoryData";
 import RepositoryIssuesHeader from "../../components/shared/RepositoryIssuesHeader";
 
+import { FavoritesContext } from "../../contexts/StorageRepositories/favorites";
+import Router, { useRouter } from "next/router";
+import { useContext, useEffect, useState } from "react";
 import { ActionButtons, Container, Content, LikeButton } from "./styles";
-import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
 import Header from "../../components/pages/Home/Header";
 import {
   EyeIcon,
@@ -16,8 +17,19 @@ import {
   StarIcon,
 } from "@primer/octicons-react";
 import Button from "../../components/shared/Button";
+import { ThemeContext } from "../../contexts/theme";
+import { ColorRing } from "react-loader-spinner";
 
 export default function Repository() {
+  const router = useRouter();
+  const {
+    repositories,
+    toggleFavorite,
+    isFavorite,
+    addFavorite,
+    removeFavorite,
+  } = useContext(FavoritesContext);
+  const { theme } = useContext(ThemeContext);
   const { repositoryName } = useRouter().query;
   const [loading, setLoading] = useState(false);
   const [repository, setRepository] = useState({});
@@ -26,32 +38,25 @@ export default function Repository() {
     if (repositoryName) {
       (async () => {
         setLoading(true);
-        // axios
-        //   .get(`https://api.github.com/repos/${repositoryName}`)
-        //   .then((response) => {
-        //     let data = {
-        //       name: response.data.name,
-        //       description: response.data.description,
-        //       owner: {
-        //         login: response.data.owner.login,
-        //         avatar_url: response.data.owner.avatar_url,
-        //       },
-        //       watchers_count: response.data.watchers_count,
-        //       forks_count: response.data.forks_count,
-        //       subscribers_count: response.data.subscribers_count,
-        //     };
-        //     setRepository(data);
-        //     setLoading(false);
-        //   });
+        axios
+          .get(`https://api.github.com/repos/${repositoryName}`)
+          .then((response) => {
+            setRepository(response.data);
+            setLoading(false);
+          });
         setRepository(repo_data);
       })();
     }
   }, [repositoryName]);
 
-  const formatValue = (n) => {
+  function formatValue(n) {
     if (n < 1e3) return n;
     if (n >= 1e3) return +(n / 1e3).toFixed(0) + "K";
-  };
+  }
+
+  function goToIssues() {
+    router.push("/issues/" + repository.full_name.replace("/", "%2F"));
+  }
 
   return (
     <>
@@ -63,33 +68,50 @@ export default function Repository() {
       {repository?.name && <RepositoryIssuesHeader repository={repository} />}
       <Container>
         <Content>
-          <img className="logo" src={repository.owner?.avatar_url} />
-          <div>
-            <p className="name">{repository.name}</p>
-            <span className="description">{repository.description}</span>
-            <div className="stats">
-              <span>
-                <StarIcon />
-                {formatValue(repository.forks_count)}
-              </span>
-              <span>
-                <RepoForkedIcon />
-                {formatValue(repository.subscribers_count)}
-              </span>
-              <span>
-                <EyeIcon />
-                {formatValue(repository.watchers_count)}
-              </span>
-            </div>
-            <ActionButtons>
-              <Button type="rounded" primary>
-                Ver issues
-              </Button>
-              <LikeButton>
-                <HeartIcon />
-              </LikeButton>
-            </ActionButtons>
-          </div>
+          {loading ? (
+            <ColorRing
+              visible={true}
+              height="60"
+              width="60"
+              ariaLabel="blocks-loading"
+              wrapperClass="blocks-wrapper"
+              colors={theme.colors.blue[200].repeat(5).match(/.{1,7}/g)}
+            />
+          ) : (
+            <>
+              <img className="logo" src={repository.owner?.avatar_url} />
+              <div>
+                <p className="name">{repository.name}</p>
+                <span className="description">{repository.description}</span>
+                <div className="stats">
+                  <span>
+                    <StarIcon />
+                    {formatValue(repository.forks_count)}
+                  </span>
+                  <span>
+                    <RepoForkedIcon />
+                    {formatValue(repository.subscribers_count)}
+                  </span>
+                  <span>
+                    <EyeIcon />
+                    {formatValue(repository.watchers_count)}
+                  </span>
+                </div>
+                <ActionButtons>
+                  <Button onClick={goToIssues} type="rounded" primary>
+                    Ver issues
+                  </Button>
+                  <LikeButton onClick={() => toggleFavorite(repository)}>
+                    {isFavorite(repository) ? (
+                      <HeartFillIcon fill={theme.colors.red} />
+                    ) : (
+                      <HeartIcon fill={theme.colors.secundary} />
+                    )}
+                  </LikeButton>
+                </ActionButtons>
+              </div>
+            </>
+          )}
         </Content>
       </Container>
     </>
