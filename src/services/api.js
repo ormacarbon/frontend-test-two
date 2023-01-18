bacdc23e74544f9a5f3be2d08f746d16a2738faa
Mateus Lopes <73197343+progmateus@users.noreply.github.com>
2023-01-18 04:14:48 -0300

    modify token api

--- src/services/api.js
+++ src/services/api.js
@@ -2,6 +2,6 @@ import axios from "axios";
 
 export const api = axios.create({
     baseURL: "https://api.cosmos.bluesoft.com.br",
-    headers: { 'X-Cosmos-Token': "kE3OdctnwL3IrT3c_ZVILw" },
+    headers: { 'X-Cosmos-Token': "qwbOLARThPUoG21lEhmE3w" },
     validateStatus: () => true
 });
