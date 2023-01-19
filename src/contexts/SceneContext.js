561393e4c25c379f4c9efd6c1d2fb68c5ff98753
taciano <tacianohora2020@hotmail.com>
2023-01-18 21:17:23 -0300

    style: delete data

--- src/contexts/SceneContext.js
+++ src/contexts/SceneContext.js
@@ -3,7 +3,7 @@ import React from "react";
 export const SceneContext = React.createContext(null);
 
 export const SceneContextProvider = ({ children }) => {
-  const [scenes, setScene] = React.useState(["cena 1", "cena 2", "point"]);
+  const [scenes, setScene] = React.useState([]);
   return (
     <SceneContext.Provider value={{ scenes, setScene }}>
       {children}
