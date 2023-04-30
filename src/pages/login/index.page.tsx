df00625c95cd2e4f25d73f7d53b983b40f9d99d3
Joseph Haase <joseph.haase.dev@gmail.com>
2023-04-30 19:03:22 -0300

    Integrates the login screen

--- src/pages/login/index.page.tsx
+++ src/pages/login/index.page.tsx
@@ -1,6 +1,9 @@
-import React, { useState, type FC } from 'react'
+import React, { useState, type FC, useEffect } from 'react'
+import { useRouter } from 'next/router'
 import Link from 'next/link'
 
+import http, { setAuthTokenAndUserID } from '../../axios/axiosConfig'
+import { validateEmail } from '../../utils/utils'
 import * as C from './LoginStyles'
 import Input from '../../components/input'
 import Button from '../../components/button'
@@ -22,10 +25,20 @@ interface LoginFormState {
 }
 
 const Login: FC = () => {
+  const router = useRouter()
+
   const [loginForm, setLoginForm] = useState<LoginForm>({ email: '', password: '' })
   const [loginFormState, setLoginFormState] = useState<LoginFormState>({ email: { state: true, feedback: '' }, password: { state: true, feedback: '' } })
   const [userNotFound, setUserNotFound] = useState<boolean>(false)
   const [disableButton, setDisableButton] = useState<boolean>(false)
+
+  useEffect(() => {
+    const userID: string = localStorage.getItem('userID')
+    const token: string = localStorage.getItem('token')
+
+    if (userID && token) router.push('/tasks')
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
 
   const handleEmailInputChange = (newValue: string): void => {
     setLoginForm({...loginForm, email: newValue})
@@ -33,6 +46,50 @@ const Login: FC = () => {
 
   const handlePasswordInputChange = (newValue: string): void => {
     setLoginForm({...loginForm, password: newValue})
+  }
+
+  const validateFields = (): boolean => {
+    Object.keys(loginForm).forEach((prop) => {
+      if (loginForm[prop] === '') {
+        loginFormState[prop].state = false
+        loginFormState[prop].feedback = 'Campo obrigatório'
+      } else {
+        loginFormState[prop].state = true
+        loginFormState[prop].feedback = ''
+
+        if (prop === 'email') {
+          const validation: boolean = validateEmail(loginForm[prop])
+
+          loginFormState[prop].state = validation
+          loginFormState[prop].feedback = validation ? '' : 'Insira um e-mail válido'
+        }
+      }
+      setLoginFormState({ ...loginFormState })
+    })
+
+    const states = Object.keys(loginFormState).map((prop) => loginFormState[prop].state)
+
+    return !states.includes(false)
+  }
+
+  const login = (): void => {
+    const validation: boolean = validateFields()
+
+    if (validation) {
+      setDisableButton(true)
+      http.post('/login', loginForm)
+        .then((response) => {
+          const { token, userID } = response.data
+          setAuthTokenAndUserID(token, userID.toString())
+          if (!userNotFound) setUserNotFound(false)
+          router.push('/tasks')
+        })
+        .catch((e): void => {
+          setUserNotFound(true)
+          setDisableButton(false)
+          console.error(e?.message)
+        })
+    }
   }
 
   return (
@@ -43,7 +100,7 @@ const Login: FC = () => {
           {userNotFound && <C.FormError>Usuário ou senha inválidos!</C.FormError>}
           <Input
             id="email-input"
-            label={'E-mail'}
+            label="E-mail"
             placeholder="Digite aqui"
             onChange={handleEmailInputChange}
             invalidFeedback={loginFormState.email.feedback}
@@ -60,7 +117,12 @@ const Login: FC = () => {
             state={loginFormState.password.state}
           />
         </C.Form>
-        <Button label="Entrar" option="salmon" disabled={disableButton} />
+        <Button
+          label="Entrar"
+           option="salmon"
+           disabled={disableButton}
+           onClick={login}
+        />
         <C.RegisterText>
           Não possui uma conta? <Link href="/register">Cadastre-se</Link>
         </C.RegisterText>
